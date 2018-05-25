54d7ac31a6fb53f672fbc5146362103af92eccdb
Cheryl Craig <cacraig@ucar.edu>
2018-05-25 13:27:28 -0600

    Move loop over solver from driver into chem_solve

--- src/chem_solve.F90
+++ src/chem_solve.F90
@@ -1,4 +1,4 @@
-module chem_solve
+module chemSolve
 
 !----------------------------------------------------------------------
 ! Module which solves the chemical equations
@@ -8,48 +8,63 @@ module chem_solve
 ! ignorant of anything about y, force(y)
 !----------------------------------------------------------------------
 
-use precision, only : r8
-use chemistry_specification, only: nSpecies_specified
-use solver_specification, only: ndiv
-use rosenbrock_integrator, only: Rosenbrock
-use forcing_and_jacobian, only: forcingParam_type
+use precision, only:                   r8
+use chemistry_specification, only:     nSpecies_specified
+use solver_specification, only:        ndiv
+use rosenbrock_integrator, only:       Rosenbrock
+use forcing_and_jacobian, only:        forcingParam_type
 
 implicit none
 
 private
-public :: chem_solve_register, chem_solve_run
+public :: chemSolve_register, chemSolve_init, chemSolve_run
 
 integer :: nSpecies
 
 contains
 
-  subroutine chem_solve_register (nSpecies_loc)
+  subroutine chemSolve_register (nSpecies_loc)
     integer, intent(out) :: nSpecies_loc ! number of chemical species (NOTE -- This needs to be "protected" in cap)
 
     nSpecies_loc = nSpecies_specified  ! Set the number of species to pass back to the driver
     nSpecies     = nSpecies_specified  ! Set the module level nSpecies
 
-  end subroutine chem_solve_register
+  end subroutine chemSolve_register
+
+  subroutine chemSolve_init (absTol, relTol)
+  !---------------------------------------------------------------
+  ! Initialize the chemistry solver
+  !---------------------------------------------------------------
+    real(r8),pointer, intent(inout) :: absTol(:) ! absolute tolerance
+    real(r8),pointer, intent(inout) :: relTol(:) ! relatvie tolerance
+
+!>>FromCafe
+    AbsTol(:) = 1.e-9_r8
+    RelTol(:) = 1.e-4_r8
+!<<FromCafe
+  
+  end subroutine chemSolve_init
+
 
   ! returns 0 for failure, 1 for success, other integers for other data
-  subroutine chem_solve_run (nkReact, state_init, timeEnd, kRateConst, AbsTol, RelTol, state_final, ierr)
+  subroutine chemSolve_run (nkReact, vmr_init, time_step_size, kRateConst, AbsTol, RelTol, vmr_curr, ierr)
 
     integer, intent(in) :: nkReact
-    real(r8), pointer, intent(in) :: state_init(:)
-    real(r8), intent(in) :: timeEnd
+    real(r8), pointer, intent(in) :: vmr_init(:)
+    real(r8), intent(in) :: time_step_size
     real(r8), pointer, intent(in) ::  kRateConst(:)   ! rates constants for each reaction
     real(r8), intent(in) :: AbsTol(:)
     real(r8), intent(in) :: RelTol(:)
-    real(r8), intent(out) :: state_final(nSpecies)
+    real(r8), intent(out) :: vmr_curr(:)
     integer, intent(out) :: ierr
    
     type(forcingParam_type) :: forcingParam
 
     integer  :: icntrl(20), istatus(20)
-    real(r8) :: state_curr(nSpecies)
     real(r8) :: rcntrl(20), rstatus(20)
  
     real(r8) :: timeStart = 0._r8
+    real(r8) :: timeEnd, time
 
     icntrl(:) = 0 ; rcntrl(:) = 0._r8
 
@@ -60,18 +75,25 @@ contains
 
     forcingParam%k_rateConst(:) = kRateConst(:)
 
-    state_curr(:) = state_init(:)
+    vmr_curr(:) = vmr_init(:)
 
     ! using rosenbrock ros3 solver
+     timeEnd   = timeStart + real(ndiv,r8) * time_step_size
+     time   = timeStart
 
-    call Rosenbrock( nSpecies, state_curr, &
-                     timeStart, timeEnd,  forcingParam, AbsTol, RelTol, &
-                     rcntrl, icntrl, rstatus, istatus, ierr )
-    state_final(:) = state_curr(:)
-   
+    ! Advance vmr for time_step_size seconds
+    do while ( time < timeEnd ) 
+        call Rosenbrock( nSpecies, vmr_curr, &
+                         timeStart, time_step_size,  forcingParam, AbsTol, RelTol, &
+                         rcntrl, icntrl, rstatus, istatus, ierr )
+
+        if( ierr /= 1 ) exit
+
+        Time = Time + time_step_size
+    end do
   
-  end subroutine chem_solve_run
+  end subroutine chemSolve_run
     
   
-end module chem_solve
+end module chemSolve
 
--- src/driver.F90
+++ src/driver.F90
@@ -4,12 +4,8 @@ program micm_driver
 ! Program which prototypes the MICM driver
 !--------------------------------------------------------------------------------
 use external_fields,     only: set_externals
-use chem_solve,          only: chem_solve_register,  chem_solve_run
+use chemSolve,          only: chemSolve_register,  chemSolve_init, chemSolve_run
 use k_rateConst_module, only: k_rateConst_register, k_rateConst_init, k_rateConst_run
-
-! This probably needs to be replaced
-use solver_specification, only: nStep => ndiv
-
 
 use precision, only : r8
 
@@ -19,14 +15,13 @@ integer :: nSpecies                    ! Number of chemical species in run
 integer :: nkReact                     ! Number of k reactions in run
 real(r8), pointer :: k_rateConst(:)
 
-real(r8),pointer :: vmr(:)
-real(r8),pointer :: advanced_vmr(:)
+real(r8),pointer :: vmr_init(:)
+real(r8),pointer :: vmr_curr(:)
 
 ! this system is unstable, so 3e+1 fails
 real(r8) :: time_step_size = 2e+1_r8 ! seconds
 
 ! convergence criteria will have to be set somewhere and passed to ode solver.
-real(r8)         :: Tstart, Tend, Time
 real(r8),pointer :: RelTol(:)
 real(r8),pointer :: AbsTol(:)
 integer :: ode_retcode
@@ -35,15 +30,15 @@ integer :: ode_retcode
 ! Register the chemistry packages
 !-----------------------------------------------
 
-call chem_solve_register(nSpecies)
+call chemSolve_register(nSpecies)
 call k_rateConst_register(nkReact)
 
 
 !-----------------------------------------------
 ! Allocate the local variables  (This will be done via CPF?)
 !-----------------------------------------------
-allocate (vmr(nSpecies))
-allocate (advanced_vmr(nSpecies))
+allocate (vmr_init(nSpecies))
+allocate (vmr_curr(nSpecies))
 allocate (AbsTol(nSpecies))
 allocate (RelTol(nSpecies))
 allocate(k_rateConst(nkReact))
@@ -53,54 +48,34 @@ allocate(k_rateConst(nkReact))
 !-----------------------------------------------
 
 call k_rateConst_init(nkReact, k_rateConst)
+call chemSolve_init(absTol, relTol)
 
 !-----------------------------------------------
 ! Explicitly specify the data which will come from the  cpf
 !-----------------------------------------------
 
-  vmr(:) =(/1._r8, 0._r8, 0._r8/)
+  vmr_init(:) =(/1._r8, 0._r8, 0._r8/)
   print *, 'initial value'
-  print *, vmr
+  print *, vmr_init
 
 
 !-----------------------------------------------
 ! Simulate the XML file which CCPP will use to drive the model
 !-----------------------------------------------
 ! Only called at beginnning
-    call k_rateConst_run(k_rateConst)
+  call k_rateConst_run(k_rateConst)
 
-  Tstart = 0._r8
-  Tend   = Tstart + real(nstep,r8) * time_step_size
-  Time   = Tstart
-  AbsTol(:) = 1.e-9_r8
-  RelTol(:) = 1.e-4_r8
-
-! Called once to advance vmr for time_step_size seconds
-    do
-      if( Time < Tend ) then
-        call  chem_solve_run(nkReact, vmr, time_step_size, k_rateConst, AbsTol, RelTol, advanced_vmr, ode_retcode)
-
-        if( ode_retcode /= 1 ) then
-          exit
-        endif
-        vmr(:) = advanced_vmr(:)
-      else
-        exit
-      endif
-      Time = Time + time_step_size
-    end do
-
+  call  chemSolve_run(nkReact, vmr_init, time_step_size, k_rateConst, AbsTol, RelTol, vmr_curr, ode_retcode)
 
 ! data back to cpf
 
   print *, 'final_value'
-  print *, advanced_vmr
+  print *, vmr_curr
 
 !-----------------------------------
 ! deallocate variables
 !-----------------------------------
 
-  deallocate (vmr, advanced_vmr, k_rateConst)
-
+  deallocate (vmr_init, vmr_curr, k_rateConst)
 
 end program micm_driver
