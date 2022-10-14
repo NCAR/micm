4d469d9b6c4ff80140792493d79455c59454e037
Jian Sun <sunjian@ucar.edu>
2022-10-14 13:27:16 -0600

    Update members in the environment class to support multi-gridpoint
    	modified:   environment.F90

--- src/environment.F90
+++ src/environment.F90
@@ -6,24 +6,25 @@ module micm_environment
 
   use kinetics_utilities,              only : number_of_photolysis_reactions
   use musica_constants,                only : musica_dk
+  use constants,                       only : ncell=>kNumberOfGridCells
 
   type environment_t
     !> Number density of air [# cm-3]
-    real(musica_dk) :: number_density_air
+    real(musica_dk) :: number_density_air(ncell)
     !> Temperature [K]
-    real(musica_dk) :: temperature
+    real(musica_dk) :: temperature(ncell)
     !> Pressure [Pa]
-    real(musica_dk) :: pressure
+    real(musica_dk) :: pressure(ncell)
     !> Aerosol surface area density \bug what are the units for surface area density?
-    real(musica_dk) :: aerosol_surface_area_density(4)
+    real(musica_dk) :: aerosol_surface_area_density(ncell,4)
     !> Aerosol diameter \bug what are the units for aerosol diameter?
-    real(musica_dk) ::  aerosol_diameter(4)
+    real(musica_dk) ::  aerosol_diameter(ncell,4)
     !> Water vapor volume mixing ratio [mol mol-1]
-    real(musica_dk) :: h2ovmr
+    real(musica_dk) :: h2ovmr(ncell)
     !> Molecular oxygen number density [# cm-3]
-    real(musica_dk) :: o2_number_density
+    real(musica_dk) :: o2_number_density(ncell)
     !> Photolysis rate constants [s-1]
-    real(musica_dk) :: photolysis_rate_constants(number_of_photolysis_reactions)
+    real(musica_dk) :: photolysis_rate_constants(ncell,number_of_photolysis_reactions)
   end type environment_t
 
 end module micm_environment
