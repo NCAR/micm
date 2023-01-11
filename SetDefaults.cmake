1ab5cacd59cd5361f5757e769544ff8f064da85a
Jian Sun <sunjian@ucar.edu>
2023-01-11 14:15:42 -0700

    Set CPU compilation flags correctly
    	modified:   SetDefaults.cmake
    	modified:   src/CMakeLists.txt
    	modified:   test/performance/CMakeLists.txt

--- SetDefaults.cmake
+++ SetDefaults.cmake
@@ -1,4 +1,4 @@
 # Overwrite the init values choosen by CMake
 if (CMAKE_Fortran_COMPILER_ID MATCHES "NVHPC")
-   set(CMAKE_Fortran_FLAGS "-O2")
+   set(CMAKE_Fortran_FLAGS "-O2 -i4 -gopt -Mextend -byteswapio -Mflushz -Kieee -Mnofma")
 endif()
