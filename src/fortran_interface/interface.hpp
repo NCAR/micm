f9b2278cadee5e797d4d2a5c749c9215cf02bf4a
Kyle Shores <kyle.shores44@gmail.com>
2023-01-19 15:56:13 -0600

    trying to call function pointer

--- src/fortran_interface/interface.hpp
+++ src/fortran_interface/interface.hpp
@@ -2,7 +2,17 @@
 
 // namespace micm {
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
   typedef void (*FuncPtr)(double *, double *, double *);
 
+
   FuncPtr get_solver(char filepath[]);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
 // }
--- test/unit/interface/interface.F90
+++ test/unit/interface/interface.F90
@@ -1,35 +1,47 @@
-module call_cpp_test
+program call_cpp_test
   use iso_c_binding
 
   implicit none
 
   interface
     type(c_funptr) function get_solver(filepath) bind(c)
-      import :: c_char
+      import :: c_char, c_funptr
       character(len=1, kind=c_char), dimension(*), intent(in) :: filepath
     end function get_solver
   end interface
 
-  ! interface
-  !   function solver(a)
-  !     import :: c_ptr
-  !     real(c_float), intent(in) :: a
-  !     real(c_float) :: func
-  !   end function
-  ! end interface
+  interface
+    function solver(arg1, arg2, arg3)
+      import :: c_ptr, c_double
+      real(c_double), dimension(*) :: arg1
+      real(c_double), dimension(*) :: arg2
+      real(c_double), dimension(*) :: arg3
+    end function
+  end interface
 
-  run()
+  call run()
 
 contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   subroutine run()
-    type(c_funptr) :: solver
+    type(c_funptr) :: csolver_func_pointer
+    procedure(solver), pointer :: fsolver
     character(len=*, kind=c_char), parameter :: filepath = "hello.txt"
 
-    print *, 'hello'
-    solver = get_solver(filepath)
+    real(c_double), dimension(:), allocatable :: arg1
+    real(c_double), dimension(:), allocatable :: arg2
+    real(c_double), dimension(:), allocatable :: arg3
+
+    allocate(arg1(5))
+    allocate(arg2(5))
+    allocate(arg3(5))
+
+    csolver_func_pointer = get_solver(filepath)
+    call c_f_procpointer(csolver_func_pointer, fsolver)
+
+    call fsolver(arg1, arg2, arg3)
   end subroutine run
 
-end module call_cpp_test
+end program call_cpp_test
