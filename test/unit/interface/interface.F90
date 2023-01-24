e064ab50e8553c23e5837521f1ee49c7e1aaca65
Kyle Shores <kyle.shores44@gmail.com>
2023-01-24 09:52:07 -0600

    making the interface be published as part of micm

--- test/unit/interface/interface.F90
+++ test/unit/interface/interface.F90
@@ -1,23 +1,8 @@
 program call_cpp_test
+  use micm_solver_interface, only: get_solver, solver
   use iso_c_binding
 
   implicit none
-
-  interface
-    type(c_funptr) function get_solver(filepath) bind(c)
-      import :: c_char, c_funptr
-      character(len=1, kind=c_char), dimension(*), intent(in) :: filepath
-    end function get_solver
-  end interface
-
-  interface
-    subroutine solver(arg1, arg2, arg3) bind(c)
-      import :: c_ptr, c_double
-      real(c_double), dimension(*) :: arg1
-      real(c_double), dimension(*) :: arg2
-      real(c_double), dimension(*) :: arg3
-    end subroutine
-  end interface
 
   call run()
 
