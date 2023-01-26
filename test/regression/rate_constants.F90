577658b39baa3bfe4ef997a90447d4f2067d85a2
Kyle Shores <kyle.shores44@gmail.com>
2023-01-26 17:12:45 -0600

    adding basic rate calculation

--- test/regression/rate_constants.F90
+++ test/regression/rate_constants.F90
@@ -1,10 +1,19 @@
 program rate_constants
+
+  use micm_environment,             only : environment_t
+  use musica_constants,             only : musica_dk
   use micm_rate_constant_arrhenius, only : rate_constant_arrhenius_t
   implicit none
 
   type(rate_constant_arrhenius_t) :: rate
+  type(environment_t)             :: env
 
-  rate = rate_constant_arrhenius_t(                                           &
-    A=real(1), B=real(1), C=real(1), D=real(1), E=real(1))
+  env%temperature = 1
+  env%pressure = 1
+
+  rate = rate_constant_arrhenius_t( A=1.0_musica_dk, B=1.0_musica_dk,         &
+    C=1.0_musica_dk, D=1.0_musica_dk, E=1.0_musica_dk)
+
+  print *, rate%calculate(env)
 
 end program rate_constants
