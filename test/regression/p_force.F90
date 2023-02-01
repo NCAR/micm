b4a7cda4f00622923f6068d5287d385400d8fbcd
Kyle Shores <kyle.shores44@gmail.com>
2023-02-01 15:10:59 -0600

    removing useless comment

--- test/regression/p_force.F90
+++ test/regression/p_force.F90
@@ -7,7 +7,6 @@ contains
 
   subroutine p_force(rate_constants, number_densities, number_density_air, force) bind(c)
     use iso_c_binding,      only : c_double
-    ! use musica_constants,             only : musica_dk
     use kinetics_utilities, only : old_p_force => p_force
 
     real(kind=c_double), intent(inout) :: rate_constants(:), number_densities(:)
