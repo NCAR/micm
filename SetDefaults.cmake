4edc1f4fa9489d117986e141de1d8abb353d3aa8
Jian Sun <sunjian@ucar.edu>
2023-02-01 11:22:45 -0700

    Update some compilation flags for TS1 mechanism
    	modified:   SetDefaults.cmake

--- SetDefaults.cmake
+++ SetDefaults.cmake
@@ -1,6 +1,6 @@
 # Overwrite the init values choosen by CMake
 if (CMAKE_Fortran_COMPILER_ID MATCHES "NVHPC")
-   set(CMAKE_Fortran_FLAGS "-O2 -i4 -gopt -Mextend -byteswapio -Mflushz -Kieee -Mnofma -Mfree -Mnobounds -mcmodel=medium")
+   set(CMAKE_Fortran_FLAGS "-O2 -Mextend -byteswapio -Mflushz -Kieee -Mnofma -Mfree -Mnobounds -mcmodel=medium -time")
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
-   set(CMAKE_Fortran_FLAGS "-O2 -ffree-form -ffree-line-length-none -fno-range-check -fPIC -mcmodel=medium")
+   set(CMAKE_Fortran_FLAGS "-O2 -ffree-form -ffree-line-length-none -fno-range-check -fPIC -mcmodel=medium -fopenmp")
 endif()
