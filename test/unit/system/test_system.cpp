d866a8043314afe1eabc441ec1d26999141a1ac2
Kyle Shores <kyle.shores44@gmail.com>
2023-04-05 16:43:54 -0600

    fixing more tests

--- test/unit/system/test_system.cpp
+++ test/unit/system/test_system.cpp
@@ -14,27 +14,6 @@ micm::SystemParameters FullSetOfParameters(){
     micm::Phase(),
   };
 
-  parameters.conditions_ = { micm::Condition("name", "units") };
-
-  parameters.photolysis_reactions_ = std::vector<micm::IntraPhaseProcess<micm::PhotolysisRateConstant>> { 
-    micm::IntraPhaseProcess<micm::PhotolysisRateConstant>(
-      std::vector<micm::Species> { micm::Species("O2") },
-      std::vector<micm::Species> { micm::Species("O"), micm::Species("O") },
-      micm::PhotolysisRateConstant(1.1)
-    )
-  };
-
-  micm::ArrheniusRateConstantParameters arrhenius_parameters;
-  arrhenius_parameters.A_ = 3.3e-11;
-  arrhenius_parameters.C_ = 55;
-  parameters.arrhenius_reactions_ = std::vector<micm::IntraPhaseProcess<micm::ArrheniusRateConstant>> {
-    micm::IntraPhaseProcess<micm::ArrheniusRateConstant>(
-      std::vector<micm::Species> { micm::Species("O1D"), micm::Species("O2") },
-      std::vector<micm::Species> { micm::Species("O"), micm::Species("O2") },
-      micm::ArrheniusRateConstant(arrhenius_parameters)
-    )
-  };
-
   return parameters;
 }
 
@@ -64,69 +43,9 @@ TEST(System, ConstructorWithMultiplePhases){
   EXPECT_EQ(system.phases_.size(), 3);
 }
 
-TEST(System, ConstructorWithCondition){
-  micm::SystemParameters parameters;
-  parameters.conditions_ = { micm::Condition("name", "units") };
-
-  micm::System system(parameters);
-
-  EXPECT_EQ(system.conditions_.size(), 1);
-  EXPECT_EQ(system.conditions_[0].name_, "name");
-  EXPECT_EQ(system.conditions_[0].units_, "units");
-}
-
-TEST(System, ConstructorWithPhotolysisReactions){
-  micm::SystemParameters parameters;
-  parameters.photolysis_reactions_ = std::vector<micm::IntraPhaseProcess<micm::PhotolysisRateConstant>> { 
-    micm::IntraPhaseProcess<micm::PhotolysisRateConstant>(
-      std::vector<micm::Species> { micm::Species("O2") },
-      std::vector<micm::Species> { micm::Species("O"), micm::Species("O") },
-      micm::PhotolysisRateConstant(1.1)
-    )
-  };
-
-  micm::System system(parameters);
-
-  EXPECT_EQ(system.photolysis_reactions_[0].rate_.rate_, 1.1);
-  EXPECT_EQ(system.photolysis_reactions_[0].reactants_.size(), 1);
-  EXPECT_EQ(system.photolysis_reactions_[0].products_.size(), 2);
-}
-
-TEST(System, ConstructorWithArrheniusReaction){
-  micm::SystemParameters parameters;
-
-  micm::ArrheniusRateConstantParameters arrhenius_parameters;
-  arrhenius_parameters.A_ = 3.3e-11;
-  arrhenius_parameters.C_ = 55;
-  parameters.arrhenius_reactions_ = std::vector<micm::IntraPhaseProcess<micm::ArrheniusRateConstant>> {
-    micm::IntraPhaseProcess<micm::ArrheniusRateConstant>(
-      std::vector<micm::Species> { micm::Species("O1D"), micm::Species("O2") },
-      std::vector<micm::Species> { micm::Species("O"), micm::Species("O2") },
-      micm::ArrheniusRateConstant(arrhenius_parameters)
-    )
-  };
-
-  micm::System system(parameters);
-
-  EXPECT_EQ(system.arrhenius_reactions_[0].rate_.parameters_.A_, 3.3e-11);
-  EXPECT_EQ(system.arrhenius_reactions_[0].rate_.parameters_.C_, 55);
-  EXPECT_EQ(system.arrhenius_reactions_[0].reactants_.size(), 2);
-  EXPECT_EQ(system.arrhenius_reactions_[0].products_.size(), 2);
-}
-
 TEST(System, ConstructorWithAllParameters){
   micm::System system{FullSetOfParameters()};
 
   EXPECT_EQ(system.gas_phase_.species_.size(), 0);
   EXPECT_EQ(system.phases_.size(), 3);
-  EXPECT_EQ(system.conditions_.size(), 1);
-  EXPECT_EQ(system.conditions_[0].name_, "name");
-  EXPECT_EQ(system.conditions_[0].units_, "units");
-  EXPECT_EQ(system.photolysis_reactions_[0].rate_.rate_, 1.1);
-  EXPECT_EQ(system.photolysis_reactions_[0].reactants_.size(), 1);
-  EXPECT_EQ(system.photolysis_reactions_[0].products_.size(), 2);
-  EXPECT_EQ(system.arrhenius_reactions_[0].rate_.parameters_.A_, 3.3e-11);
-  EXPECT_EQ(system.arrhenius_reactions_[0].rate_.parameters_.C_, 55);
-  EXPECT_EQ(system.arrhenius_reactions_[0].reactants_.size(), 2);
-  EXPECT_EQ(system.arrhenius_reactions_[0].products_.size(), 2);
 }
