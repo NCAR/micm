4ed949c4cc36f50eaa395b88dbdd104a944aa213
Kyle Shores <kshores@ucar.edu>
2023-06-01 09:24:25 -0600

    moving fetch content include to top

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,3 +1,5 @@
+include(FetchContent)
+
 ################################################################################
 # Memory check
 
@@ -44,11 +46,10 @@ if(PROJECT_IS_TOP_LEVEL)
     set("CMAKE_${lang}_CLANG_TIDY" "")
   endforeach ()
 
-  include(FetchContent)
   FetchContent_Declare(googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG release-1.12.1
-    FIND_PACKAGE_ARGS GTest
+    GIT_TAG v1.13.0
+    # FIND_PACKAGE_ARGS GTest
   )
 
   set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
