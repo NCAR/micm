70c9a2cb614eb10fc6f81e0fe43ccb1c4e30d140
Kyle Shores <kyle.shores44@gmail.com>
2023-06-09 12:13:39 -0500

    thing

--- test/unit/process/test_troe_rate_constant.cpp
+++ test/unit/process/test_troe_rate_constant.cpp
@@ -9,10 +9,10 @@ TEST(TroeRateConstant, CalculateWithMinimalArugments)
   state.conditions_[0].temperature_ = 301.24;  // [K]
   state.conditions_[0].air_density_ = 1.0;     // [mol mol-1]
   std::vector<double>::const_iterator params = state.custom_rate_parameters_[0].begin();
-  micm::TroeRateConstantParameters params;
-  params.k0_A_ = 1.0; 
-  params.kinf_A_ = 1.0;
-  micm::TroeRateConstant troe{params};
+  micm::TroeRateConstantParameters troe_params;
+  troe_params.k0_A_ = 1.0; 
+  troe_params.kinf_A_ = 1.0;
+  micm::TroeRateConstant troe{troe_params};
   auto k = troe.calculate(state.conditions_[0], params);
   double k0 = 1.0;
   double kinf = 1.0;
