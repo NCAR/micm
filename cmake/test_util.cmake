e745fce0de181a50cc91a8c4162cfc79c5a3ffdf
Kyle Shores <kshores@ucar.edu>
2023-06-09 15:44:50 -0600

    removing unneeded thign

--- cmake/test_util.cmake
+++ cmake/test_util.cmake
@@ -27,31 +27,6 @@ function(create_standard_test)
 endfunction(create_standard_test)
 
 ################################################################################
-# build and add an HPC test (one linked to the micm library but not google test)
-function(create_hpc_test)
-  set(prefix TEST)
-  set(singleValues NAME WORKING_DIRECTORY)
-  set(multiValues SOURCES)
-  include(CMakeParseArguments)
-  cmake_parse_arguments(${prefix} " " "${singleValues}" "${multiValues}" ${ARGN})
-  add_executable(test_${TEST_NAME} ${TEST_SOURCES})
-  target_link_libraries(test_${TEST_NAME} PUBLIC musica::micm)
-
-  if (CMAKE_CXX_COMPILER_ID MATCHES "NVHPC")
-    target_link_libraries(test_${TEST_NAME} PUBLIC stdc++)
-  endif()
-
-  if(ENABLE_JSON)
-    target_link_libraries(test_${TEST_NAME} PRIVATE nlohmann_json::nlohmann_json)
-    target_compile_definitions(test_${TEST_NAME} PUBLIC USE_JSON)
-  endif()
-  if(NOT DEFINED TEST_WORKING_DIRECTORY)
-    set(TEST_WORKING_DIRECTORY "${CMAKE_BINARY_DIR}")
-  endif()
-  add_micm_test(${TEST_NAME} test_${TEST_NAME} "" ${TEST_WORKING_DIRECTORY})
-endfunction(create_hpc_test)
-
-################################################################################
 # Add a test
 
 function(add_micm_test test_name test_binary test_args working_dir)
