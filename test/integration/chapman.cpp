d7d5cc1377f6912eb5c67b922c1769e3b7cba133
Jiwon Gim <jiwongim@ucar.edu>
2023-06-16 16:06:09 -0600

    add an assign function in sparse matrix class and chapman integration test using config

--- test/integration/chapman.cpp
+++ test/integration/chapman.cpp
@@ -12,6 +12,42 @@
 
 using yields = std::pair<micm::Species, double>;
 
+#ifdef USE_JSON
+#  include <micm/configure/solver_config.hpp>
+TEST(ChapmanIntegration, CanBuildChapmanSystemUsingConfig)
+{
+  micm::SolverConfig<micm::JsonReaderPolicy, micm::ThrowPolicy> solverConfig{};  // Throw policy
+  std::variant<micm::SolverParameters, micm::ConfigErrorCode> configs =
+      solverConfig.Configure("./unit_configs/chapman/config.json");
+
+  // Check if parsing is successful and returns 'Solverparameters'
+  auto* solver_params_ptr = std::get_if<micm::SolverParameters>(&configs);
+  EXPECT_TRUE(solver_params_ptr != nullptr);
+
+  micm::SolverParameters& solver_params = *solver_params_ptr;
+
+  micm::RosenbrockSolver solver{ solver_params.system_,
+                                 std::move(solver_params.processes_),
+                                 micm::RosenbrockSolverParameters{} };
+
+  micm::State state = solver.GetState();
+
+  std::vector<double> concentrations{ 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.3, 0.3, 0.3 };
+  state.variables_[0] = concentrations;
+  std::vector<double> photo_rates{ 0.1, 0.2, 0.3 };
+  state.custom_rate_parameters_[0] = photo_rates;
+  state.conditions_[0].temperature_ = 2;
+  state.conditions_[0].pressure_ = 3;
+
+  for (double t{}; t < 100; ++t)
+  {
+    state.custom_rate_parameters_[0] = photo_rates;
+    auto result = solver.Solve(t, t + 0.5, state);
+    // output state
+  }
+}
+#endif
+
 TEST(ChapmanIntegration, CanBuildChapmanSystem)
 {
   auto o = micm::Species("O");
@@ -26,33 +62,32 @@ TEST(ChapmanIntegration, CanBuildChapmanSystem)
 
   micm::Phase gas_phase{ std::vector<micm::Species>{ o, o1d, o2, o3, m, ar, n2, h2o, co2 } };
 
-  micm::Process r1 =
-      micm::Process::create()
-          .reactants({ o1d, n2 })
-          .products({ yields(o, 1), yields(n2, 1) })
-          .rate_constant(micm::ArrheniusRateConstant({ .A_ = 2.15e-11, .B_= 0, .C_ = 110 }))
-          .phase(gas_phase);
+  micm::Process r1 = micm::Process::create()
+                         .reactants({ o1d, n2 })
+                         .products({ yields(o, 1), yields(n2, 1) })
+                         .rate_constant(micm::ArrheniusRateConstant({ .A_ = 2.15e-11, .B_ = 0, .C_ = 110 }))
+                         .phase(gas_phase);
 
-  micm::Process r2 =
-      micm::Process::create()
-          .reactants({ o1d, o2 })
-          .products({ yields(o, 1), yields(o2, 1) })
-          .rate_constant(micm::ArrheniusRateConstant(micm::ArrheniusRateConstantParameters{ .A_ = 3.3e-11, .B_ = 0, .C_ = 55 }))
-          .phase(gas_phase);
+  micm::Process r2 = micm::Process::create()
+                         .reactants({ o1d, o2 })
+                         .products({ yields(o, 1), yields(o2, 1) })
+                         .rate_constant(micm::ArrheniusRateConstant(
+                             micm::ArrheniusRateConstantParameters{ .A_ = 3.3e-11, .B_ = 0, .C_ = 55 }))
+                         .phase(gas_phase);
 
-  micm::Process r3 =
-      micm::Process::create()
-          .reactants({ o, o3 })
-          .products({ yields(o2, 2) })
-          .rate_constant(micm::ArrheniusRateConstant(micm::ArrheniusRateConstantParameters{ .A_ = 8e-12, .B_ = 0, .C_ = -2060 }))
-          .phase(gas_phase);
+  micm::Process r3 = micm::Process::create()
+                         .reactants({ o, o3 })
+                         .products({ yields(o2, 2) })
+                         .rate_constant(micm::ArrheniusRateConstant(
+                             micm::ArrheniusRateConstantParameters{ .A_ = 8e-12, .B_ = 0, .C_ = -2060 }))
+                         .phase(gas_phase);
 
-  micm::Process r4 =
-      micm::Process::create()
-          .reactants({ o, o2, m })
-          .products({ yields(o3, 1), yields(m, 1) })
-          .rate_constant(micm::ArrheniusRateConstant(micm::ArrheniusRateConstantParameters{ .A_ = 6.0e-34, .B_ = 0, .C_ = 2.4 }))
-          .phase(gas_phase);
+  micm::Process r4 = micm::Process::create()
+                         .reactants({ o, o2, m })
+                         .products({ yields(o3, 1), yields(m, 1) })
+                         .rate_constant(micm::ArrheniusRateConstant(
+                             micm::ArrheniusRateConstantParameters{ .A_ = 6.0e-34, .B_ = 0, .C_ = 2.4 }))
+                         .phase(gas_phase);
 
   micm::Process photo_1 = micm::Process::create()
                               .reactants({ o2 })
