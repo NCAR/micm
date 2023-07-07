d0e98dac56c9fdb37f9192a8f23b769e52a551ce
Matt Dawson <mattdawson@ucar.edu>
2023-07-07 14:03:30 -0700

    add sparse matrix policy to RosenbrockSolver class

--- test/unit/solver/test_rosenbrock.cpp
+++ test/unit/solver/test_rosenbrock.cpp
@@ -3,8 +3,9 @@
 #include <micm/solver/rosenbrock.hpp>
 #include <micm/solver/solver.hpp>
 #include <micm/util/matrix.hpp>
+#include <micm/util/sparse_matrix.hpp>
 
 TEST(ChapmanODESolver, DefaultConstructor)
 {
-  micm::RosenbrockSolver<micm::Matrix> solver{};
+  micm::RosenbrockSolver<micm::Matrix, micm::SparseMatrix> solver{};
 }
