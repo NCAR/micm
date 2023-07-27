1420995c259c27e7b2cd0948381d1ca3edfb885c
dwfncar <fillmore@ucar.edu>
2023-07-27 15:44:52 -0600

    Start on kpp_to_micm.py script to convert KPP configs to MICM JSON configs.

---
+++ etc/scripts/kpp_to_micm.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import argparse
+import logging
+
+if __name__ == '__main__':
+
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--logfile', type=str,
+        default=sys.stdout,
+        help='log file (default stdout)')
+    parser.add_argument('--debug', action='store_true',
+        help='set logging level to debug')
+    args = parser.parse_args()
+
+    """
+    Setup logging
+    """
+    logging_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(stream=args.logfile, level=logging_level)
