504c6a21c7d149653230fe16a1ec084bb314a20a
Matt Dawson <mattdawson@ucar.edu>
2023-07-27 16:51:33 -0700

    add branched reaction and tests

--- include/micm/util/constants.hpp
+++ include/micm/util/constants.hpp
@@ -1,1 +1,7 @@
+// Copyright (C) 2023 National Center for Atmospheric Research,
+//
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
 static constexpr double BOLTZMANN_CONSTANT = 1.3806505e-23;  // J K^{-1}
+static constexpr double AVOGADRO_CONSTANT = 6.02214076e23;   // # mol^{-1}
