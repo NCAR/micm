6f150423609ed1d164f6d2bba5840c129c0a2286
dwfncar <fillmore@ucar.edu>
2023-07-28 09:33:55 -0600

    Start on read_kpp_config method, introduced KPP config file suffixes.

--- etc/scripts/kpp_to_micm.py
+++ etc/scripts/kpp_to_micm.py
@@ -2,6 +2,26 @@ import os
 import sys
 import argparse
 import logging
+import json
+from glob import glob
+
+
+def read_kpp_config(kpp_dir):
+    """
+    Read all KPP config files in a directory
+
+    Parameters
+        kpp_dir (str): KPP directory
+
+    Returns
+    """
+
+    kpp_suffixes = ['.kpp', '.spc', '.eqn', '.def']
+
+    for suffix in kpp_suffixes:
+        files = glob(os.path.join(kpp_dir, '*' + suffix))
+        logging.debug(files)
+
 
 if __name__ == '__main__':
 
@@ -12,6 +32,9 @@ if __name__ == '__main__':
     parser.add_argument('--logfile', type=str,
         default=sys.stdout,
         help='log file (default stdout)')
+    parser.add_argument('--kpp_dir', type=str,
+        default=os.path.join('..', 'configs', 'kpp'),
+        help='KPP config directory')
     parser.add_argument('--debug', action='store_true',
         help='set logging level to debug')
     args = parser.parse_args()
@@ -21,3 +44,6 @@ if __name__ == '__main__':
     """
     logging_level = logging.DEBUG if args.debug else logging.INFO
     logging.basicConfig(stream=args.logfile, level=logging_level)
+
+    read_kpp_config(args.kpp_dir)
+
