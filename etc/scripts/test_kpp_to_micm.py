ede0b2c5e27572432abc9dc70426041a3892d9b5
dwfncar <fillmore@ucar.edu>
2023-08-13 09:30:26 -0600

    Added ARR_ab, ARR_ac, ARR_abc parse unit tests.

--- etc/scripts/test_kpp_to_micm.py
+++ etc/scripts/test_kpp_to_micm.py
@@ -1,7 +1,16 @@
-import logging
-import kpp_to_micm
+"""
+Copyright (C) 2023
+National Center for Atmospheric Research,
+SPDX-License-Identifier: Apache-2.0
 
-logger = logging.getLogger(__name__)
+File:
+    test_kpp_to_micm.py
+
+Usage:
+    pytest test_kpp_to_micm.py --log-cli-level=DEBUG
+"""
+
+import kpp_to_micm
 
 def test_parse_kpp_arrhenius():
     """
@@ -10,7 +19,15 @@ def test_parse_kpp_arrhenius():
     ARR_ac(1.0e-12, -3.0)
     ARR_abc(1.0e-12, 2000.0, -3.0)
     """
-    kpp_str = 'ARR_ab(1.0e-12, 2000.0)'
-    logger.debug(kpp_str)
-    kpp_to_micm.parse_kpp_arrhenius(kpp_str)
+
+    kpp_A, kpp_B, kpp_C = 1.0e-12, 2000.0, -3.0
+
+    kpp_to_micm.parse_kpp_arrhenius('ARR_ab(%.2e, %.2f)' % (kpp_A, kpp_B))
     assert True
+
+    kpp_to_micm.parse_kpp_arrhenius('ARR_ac(%.2e, %.2f)' % (kpp_A, kpp_C))
+    assert True
+
+    kpp_to_micm.parse_kpp_arrhenius('ARR_abc(%.2e, %.2f, %.2f)' % (kpp_A, kpp_B, kpp_C))
+    assert True
+
