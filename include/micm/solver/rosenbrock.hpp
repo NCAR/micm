c76a2a87fa013f50e17e4892804750d5ab8b329e
Kyle Shores <kyle.shores44@gmail.com>
2023-08-22 11:06:32 -0500

    lowering number of steps

--- include/micm/solver/rosenbrock.hpp
+++ include/micm/solver/rosenbrock.hpp
@@ -41,7 +41,7 @@ namespace micm
   {
     size_t stages_{};
     size_t upper_limit_tolerance_{};
-    size_t max_number_of_steps_{ 200000 };
+    size_t max_number_of_steps_{ 1000 };
 
     double round_off_{ std::numeric_limits<double>::epsilon() };  // Unit roundoff (1+round_off)>1
     double factor_min_{ 0.2 };                                    // solver step size minimum boundary
@@ -763,10 +763,8 @@ namespace micm
 
       bool accepted = false;
       //  Repeat step calculation until current step accepted
-      while (!accepted)
+      while (!accepted & (stats_.number_of_steps > parameters_.max_number_of_steps_))
       {
-        if (stats_.number_of_steps > parameters_.max_number_of_steps_)
-          break;
         bool is_singular{ false };
         // Form and factor the rosenbrock ode jacobian
         LinearFactor(H, parameters_.gamma_[0], is_singular, Y, state.rate_constants_);
