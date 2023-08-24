f43aa9c15b93009d850a2cb3acb55b9cdcf42eff
Qina Tan <qina.tan@hotmail.com>
2023-08-24 08:06:36 -0600

    debug

--- include/micm/util/cuda_matrix_param.hpp
+++ include/micm/util/cuda_matrix_param.hpp
@@ -3,7 +3,7 @@
 namespace micm{
     class CUDAMatrixParam{
         public: 
-        double* rate_constants; 
+        const double* rate_constants; 
         template<size_t L>
         CUDAMatrixParam(const VectorMatrix<double, L>& rateConstants){
             rate_constants = rateConstants.AsVector().data(); 
