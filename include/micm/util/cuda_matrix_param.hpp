e48e36daf7669efc299bc4e3f8b9c954b7ef24b2
Qina Tan <qina.tan@hotmail.com>
2023-08-25 10:24:55 -0600

    deleted const

--- include/micm/util/cuda_matrix_param.hpp
+++ include/micm/util/cuda_matrix_param.hpp
@@ -7,14 +7,13 @@ namespace micm{
         const double* state_variables_; 
         double* forcing_; 
         double* jacobian_; 
-        const size_t n_grids_; 
-        const size_t n_reactions_; 
-        const size_t n_species_; 
+        size_t n_grids_; 
+        size_t n_reactions_; 
+        size_t n_species_; 
         size_t jacobian_size_; 
-        bool set_n_species ; 
         
         CUDAMatrixParam(){
-            set_n_species = false; 
+        
         }; 
         inline void setGrids(size_t n_grids){
             n_grids_ = n_grids; 
@@ -25,22 +24,18 @@ namespace micm{
         }; 
         inline void setStateVariables(std::vector<double>& state_variables, size_t n_species){
             state_variables_ = state_variables.data(); 
-            if (set_n_species == false){
             n_species_ = n_species; 
-            set_n_species = true; 
-            }
         }; 
+
         inline void setForcing(std::vector<double>& forcing, size_t n_species){
             forcing_ = forcing.data(); 
-            if (set_n_species == false){
             n_species_ = n_species; 
-            set_n_species = true; 
-            }
         }; 
+        
         inline void setJacobian(std::vector<double>& jacobian, size_t jacobian_size){
             jacobian_ = jacobian.data(); 
             jacobian_size_ = jacobian_size; 
-        }
+        }; 
 
     }; //end class
 }//end micm
