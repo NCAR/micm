9c83acb19380e2a2f13dbbf582dee1e7282dda3f
Qina Tan <qina.tan@hotmail.com>
2023-09-21 10:39:00 -0600

    test

--- src/solver/lu_decomposition.cu
+++ src/solver/lu_decomposition.cu
@@ -22,10 +22,9 @@ namespace micm{
         __global__ void pairCheck(double* d_A, size_t A_size){
             size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
             if (tid < A_size){
-            printf("this is A value: %g\n", d_A[tid]);
+            printf("this is A value: %16.12f\n", d_A[tid]);
         }
     }
-        
         
         __global__ void DecomposeKernel(
             decomposeDevice* device,
