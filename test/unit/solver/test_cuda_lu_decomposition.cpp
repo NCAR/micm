5bbdd201463e705a8169c434dbf5c5d4aad83660
Qina Tan <qina.tan@hotmail.com>
2023-09-24 13:58:06 -0600

    more print

--- test/unit/solver/test_cuda_lu_decomposition.cpp
+++ test/unit/solver/test_cuda_lu_decomposition.cpp
@@ -74,11 +74,11 @@ cpu_lud.Decompose<double, SparseMatrixPolicy>(A, cpu_LU.first, cpu_LU.second);
 check_results<double, SparseMatrixPolicy>(
     A, cpu_LU.first, cpu_LU.second, [&](const double a, const double b) -> void { EXPECT_NEAR(a, b, 1.0e-5); });
  
-  micm::CUDALuDecomposition gpu_lud(A); 
-  auto gpu_LU = micm::CUDALuDecomposition::GetLUMatrices(A, 1.0e-30); 
-  gpu_lud.Decompose<double, SparseMatrixPolicy>(A, gpu_LU.first, gpu_LU.second); 
-  check_results<double, SparseMatrixPolicy>(
-      A, gpu_LU.first, gpu_LU.second, [&](const double a, const double b) -> void { EXPECT_NEAR(a, b, 1.0e-5); });
+  // micm::CUDALuDecomposition gpu_lud(A); 
+  // auto gpu_LU = micm::CUDALuDecomposition::GetLUMatrices(A, 1.0e-30); 
+  // gpu_lud.Decompose<double, SparseMatrixPolicy>(A, gpu_LU.first, gpu_LU.second); 
+  // check_results<double, SparseMatrixPolicy>(
+  //     A, gpu_LU.first, gpu_LU.second, [&](const double a, const double b) -> void { EXPECT_NEAR(a, b, 1.0e-5); });
 
   // double* A_vector = A.AsVector().data(); 
 
