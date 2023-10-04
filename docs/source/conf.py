f290f55dc1e75302a845e44faba32e60460a3aa1
Kyle Shores <kyle.shores44@gmail.com>
2023-10-04 16:21:43 -0500

    trying to correct versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,9 +21,9 @@ project = 'MICM'
 copyright = f'2022-{datetime.datetime.now().year}, NCAR/UCAR'
 author = 'NCAR/UCAR'
 
-# The full version, including alpha/beta/rc tags
-release = '3.2.0'
-
+suffix = os.getenv("SWITCHER_SUFFIX", "")
+# the suffix is required. This is controlled by the dockerfile that builds the docs
+release = f'v3.2.0{suffix}'
 
 # -- General configuration ---------------------------------------------------
 
