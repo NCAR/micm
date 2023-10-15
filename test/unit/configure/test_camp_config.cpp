c31d2e2d7e3ddb6bf286ffa50da0c79d5a7a6e1d
David Fillmore <fillmore@ucar.edu>
2023-10-15 10:41:37 -0600

    Added ReadAndParseCAMPFiles test.

--- test/unit/configure/test_camp_config.cpp
+++ test/unit/configure/test_camp_config.cpp
@@ -8,3 +8,13 @@ TEST(SolverConfig, DetectsInvalidConfigFile)
   auto status = solverConfig.ReadAndParse("not_a_config_file_directory");
   EXPECT_EQ(micm::ConfigParseStatus::InvalidCAMPFilePath, status);
 }
+
+TEST(SolverConfig, ReadAndParseCAMPFiles)
+{
+  micm::SolverConfig solverConfig{};
+
+  // Read and parse the CAMP configure file
+  micm::ConfigParseStatus status = solverConfig.ReadAndParse("./unit_configs/CAMP/camp_valid");
+  EXPECT_EQ(micm::ConfigParseStatus::Success, status);
+}
+
