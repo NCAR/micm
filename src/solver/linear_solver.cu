33271507facfd4eabb1b9a01012ca7a0b020166d
Qina Tan <qina.tan@hotmail.com>
2023-10-20 10:08:58 -0600

    added function call

--- src/solver/linear_solver.cu
+++ src/solver/linear_solver.cu
@@ -133,7 +133,7 @@ __global__ void SolveKernel(SolveDevice* device)
     
     //kernel call 
     size_t num_block = (denseMatrix.n_grids_ + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    AddForcingTermsKernel<<<num_block, BLOCK_SIZE>>>(device);
+    Solve<<<num_block, BLOCK_SIZE>>>(device);
     cudaDeviceSynchronize();
 
     //clean up 
