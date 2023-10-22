37b150eaad6dae1dabf18497883a5c770686bb05
Qina Tan <qina.tan@hotmail.com>
2023-10-22 09:50:58 -0600

    modified constructor

--- test/unit/solver/test_cuda_linear_solver.cpp
+++ test/unit/solver/test_cuda_linear_solver.cpp
@@ -34,7 +34,7 @@ TEST(CudaLinearSolver, DenseMatrixVectorOrdering)
   testDenseMatrix<Group1VectorMatrix, Group1SparseVectorMatrix, micm::CudaLinearSolver<double, Group1SparseVectorMatrix, micm::CudaLuDecomposition>>(
       [](const Group1SparseVectorMatrix<double>& matrix,
          double initial_value) -> micm::CudaLinearSolver<double, Group1SparseVectorMatrix, micm::CudaLuDecomposition> {
-        return micm::CudaLinearSolver<double, Group1SparseVectorMatrix, micm::CudaLuDecomposition>{ matrix, initial_value };
+        return micm::CudaLinearSolver<double, Group1SparseVectorMatrix>{ matrix, initial_value };
       });
 }
 
