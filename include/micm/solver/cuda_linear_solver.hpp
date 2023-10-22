29c14037f5454d293494221b9369d1c1f600c64e
Qina Tan <qina.tan@hotmail.com>
2023-10-22 12:52:07 -0600

    test

--- include/micm/solver/cuda_linear_solver.hpp
+++ include/micm/solver/cuda_linear_solver.hpp
@@ -14,44 +14,35 @@ namespace micm{
         CudaLinearSolver(){};
     
         CudaLinearSolver(const SparseMatrixPolicy<T>& matrix, T initial_value): LinearSolver<T, SparseMatrixPolicy, LuDecompositionPolicy = LuDecomposition>(matrix, initial_value){};
-        // {
-        //     //call instructor of parent class 
-        //     micm::LinearSolver<T, SparseMatrixPolicy, LuDecompositionPolicy>::LinearSolver(matrix, initial_value);
-        // };
-    
+      
         template<template<class> class MatrixPolicy> 
         requires(VectorizableDense<MatrixPolicy<T>> || VectorizableSparse<SparseMatrixPolicy<T>>)
-        void Solve(const MatrixPolicy<T>&b, MatrixPolicy<T>& x);
+        void Solve(const MatrixPolicy<T>&b, MatrixPolicy<T>& x)
+        {
+            CudaLinearSolverParam linearSolver; 
+            CudaSparseMatrixParam sparseMatrix; 
+            CudaMatrixParam denseMatrix; 
+            linearSolver.nLij_Lii_ = nLij_Lii_.data(); 
+            linearSolver.nLij_Lii_size_ = nLij_Lii_.size(); 
+            linearSolver.Lij_yj_= Lij_yj_.data();
+            linearSolver.Lij_yj_size_ = Lij_yj_.size();
+            linearSolver.nUij_Uii_ = nUij_Uii_.data();
+            linearSolver.nUij_Uii_size_ = nUij_Uii_.size();
+            linearSolver.Uij_xj_ = Uij_xj_.data(); 
+            linearSolver.Uij_xj_size_ = Uij_xj_.size(); 
+            sparseMatrix.lower_matrix_ = lower_matrix_.AsVector().data(); 
+            sparseMatrix.lower_matrix_size_ = lower_matrix_.AsVector().size();
+            sparseMatrix.upper_matrix_ = upper_matrix_.AsVector().data(); 
+            sparseMatrix.upper_matrix_size_ = upper_matrix_.AsVector().size(); 
+            denseMatrix.b_ = b.AsVector().data(); 
+            denseMatrix.x_ = x.AsVector().data();
+            denseMatrix.n_grids_ = b.size(); //number of grids
+            denseMatrix.b_column_counts_ = b[0].size(); 
+            denseMatrix.x_column_counts_= x[0].size(); 
+            
+            //calling kernel driver
+            micm::cuda::SolveKernelDriver(linearSolver, sparseMatrix, denseMatrix);
+        };
     };
-
-    template<typename T, template<class> class SparseMatrixPolicy, class LuDecompositionPolicy>
-    template<template<class> class MatrixPolicy> 
-    requires(VectorizableDense<MatrixPolicy<T>> || VectorizableSparse<SparseMatrixPolicy<T>>)
-    inline void micm::CudaLinearSolver<T, SparseMatrixPolicy, LuDecompositionPolicy>::Solve(const MatrixPolicy<T>&b, MatrixPolicy<T>& x)
-    {
-        CudaLinearSolverParam linearSolver; 
-        CudaSparseMatrixParam sparseMatrix; 
-        CudaMatrixParam denseMatrix; 
-        linearSolver.nLij_Lii_ = nLij_Lii_.data(); 
-        linearSolver.nLij_Lii_size_ = nLij_Lii_.size(); 
-        linearSolver.Lij_yj_= Lij_yj_.data();
-        linearSolver.Lij_yj_size_ = Lij_yj_.size();
-        linearSolver.nUij_Uii_ = nUij_Uii_.data();
-        linearSolver.nUij_Uii_size_ = nUij_Uii_.size();
-        linearSolver.Uij_xj_ = Uij_xj_.data(); 
-        linearSolver.Uij_xj_size_ = Uij_xj_.size(); 
-        sparseMatrix.lower_matrix_ = lower_matrix_.AsVector().data(); 
-        sparseMatrix.lower_matrix_size_ = lower_matrix_.AsVector().size();
-        sparseMatrix.upper_matrix_ = upper_matrix_.AsVector().data(); 
-        sparseMatrix.upper_matrix_size_ = upper_matrix_.AsVector().size(); 
-        denseMatrix.b_ = b.AsVector().data(); 
-        denseMatrix.x_ = x.AsVector().data();
-        denseMatrix.n_grids_ = b.size(); //number of grids
-        denseMatrix.b_column_counts_ = b[0].size(); 
-        denseMatrix.x_column_counts_= x[0].size(); 
-        
-        //calling kernel driver
-        micm::cuda::SolveKernelDriver(linearSolver, sparseMatrix, denseMatrix);
-    }
 }//end micm
 #endif
