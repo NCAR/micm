c984438711f2362c39cc1c3dd104658a735f871e
David Fillmore <fillmore@ucar.edu>
2023-10-23 21:23:02 -0600

    Removed CAMP invalid file unit test.

--- test/unit/configure/test_camp_config.cpp
+++ test/unit/configure/test_camp_config.cpp
@@ -2,15 +2,6 @@
 
 #include <micm/configure/camp_config.hpp>
 
-/*
-TEST(SolverConfig, DetectsInvalidConfigFile)
-{
-  micm::SolverConfig solverConfig{};
-  auto status = solverConfig.ReadAndParse("not_a_config_file_directory");
-  EXPECT_EQ(micm::ConfigParseStatus::InvalidCAMPFilePath, status);
-}
-*/
-
 TEST(SolverConfig, ReadAndParseCAMPFiles)
 {
   micm::SolverConfig solverConfig{};
