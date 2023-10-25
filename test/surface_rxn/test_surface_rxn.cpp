5ee2a66afbbe7b18a741f0ca8483cf341cdecd42
David Fillmore <fillmore@ucar.edu>
2023-10-25 14:36:20 -0600

    Added test/surface_rxt subdir.

---
+++ test/surface_rxn/test_surface_rxn.cpp
@@ -0,0 +1,11 @@
+#include <micm/configure/solver_config.hpp>
+#include <micm/solver/rosenbrock.hpp>
+#include <micm/system/species.hpp>
+
+
+int main(const int argc, const char* argv[])
+{
+  micm::SolverConfig solver_config;
+
+  return 0;
+}
