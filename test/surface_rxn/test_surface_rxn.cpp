219f87c8f4c740f8bf3796a200403c600b769baf
David Fillmore <fillmore@ucar.edu>
2023-10-25 18:05:28 -0600

    Start with CAMP JSON.

--- test/surface_rxn/test_surface_rxn.cpp
+++ test/surface_rxn/test_surface_rxn.cpp
@@ -1,11 +1,16 @@
 #include <micm/configure/solver_config.hpp>
-#include <micm/solver/rosenbrock.hpp>
+#include <micm/system/system.hpp>
+#include <micm/solver/state.hpp>
 #include <micm/system/species.hpp>
+#include <micm/process/surface_rate_constant.hpp>
+#include <micm/solver/rosenbrock.hpp>
 
+// based on CAMP/test/unit_rxn_data/test_rxn_surface.F90
 
 int main(const int argc, const char* argv[])
 {
   micm::SolverConfig solver_config;
+
 
   return 0;
 }
