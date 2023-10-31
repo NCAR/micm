a9f5bac8d47dd3ed879a8368922dc506c13d8db9
Qina Tan <qina.tan@hotmail.com>
2023-10-31 08:20:34 -0600

    ready to test

--- include/micm/solver/cuda_linear_solver.hpp
+++ include/micm/solver/cuda_linear_solver.hpp
@@ -8,12 +8,12 @@
 #include <micm/solver/cuda_linear_solver.cuh>
 namespace micm{
     template<typename T, template<class> class SparseMatrixPolicy, class LuDecompositionPolicy = CudaLuDecomposition>
-    class CudaLinearSolver : public LinearSolver<T, SparseMatrixPolicy, CudaLuDecomposition> {
+    class CudaLinearSolver : public LinearSolver<T, SparseMatrixPolicy, LuDecompositionPolicy> {
     public:
         //constructor
         CudaLinearSolver(){};
 
-        CudaLinearSolver(const SparseMatrixPolicy<T>& matrix, T initial_value): LinearSolver<T, SparseMatrixPolicy, CudaLuDecomposition> (matrix, initial_value){};
+        CudaLinearSolver(const SparseMatrixPolicy<T>& matrix, T initial_value): LinearSolver<T, SparseMatrixPolicy, LuDecompositionPolicy> (matrix, initial_value){};
       
         template<template<class> class MatrixPolicy> 
         requires(VectorizableDense<MatrixPolicy<T>> || VectorizableSparse<SparseMatrixPolicy<T>>)
@@ -22,18 +22,18 @@ namespace micm{
             CudaLinearSolverParam linearSolver;
             CudaSparseMatrixParam sparseMatrix; 
             CudaMatrixParam denseMatrix;
-            linearSolver.nLij_Lii_ = nLij_Lii_.data(); 
-            linearSolver.nLij_Lii_size_ = nLij_Lii_.size(); 
-            linearSolver.Lij_yj_= Lij_yj_.data();
-            linearSolver.Lij_yj_size_ = Lij_yj_.size();
-            linearSolver.nUij_Uii_ = nUij_Uii_.data();
-            linearSolver.nUij_Uii_size_ = nUij_Uii_.size();
-            linearSolver.Uij_xj_ = Uij_xj_.data(); 
-            linearSolver.Uij_xj_size_ = Uij_xj_.size(); 
-            sparseMatrix.lower_matrix_ = lower_matrix_.AsVector().data(); 
-            sparseMatrix.lower_matrix_size_ = lower_matrix_.AsVector().size();
-            sparseMatrix.upper_matrix_ = upper_matrix_.AsVector().data(); 
-            sparseMatrix.upper_matrix_size_ = upper_matrix_.AsVector().size(); 
+            linearSolver.nLij_Lii_ = this->nLij_Lii_.data(); 
+            linearSolver.nLij_Lii_size_ = this->nLij_Lii_.size(); 
+            linearSolver.Lij_yj_= this->Lij_yj_.data();
+            linearSolver.Lij_yj_size_ = this->Lij_yj_.size();
+            linearSolver.nUij_Uii_ = this->nUij_Uii_.data();
+            linearSolver.nUij_Uii_size_ = this->nUij_Uii_.size();
+            linearSolver.Uij_xj_ = this->Uij_xj_.data(); 
+            linearSolver.Uij_xj_size_ = this->Uij_xj_.size(); 
+            sparseMatrix.lower_matrix_ = this->lower_matrix_.AsVector().data(); 
+            sparseMatrix.lower_matrix_size_ = this->lower_matrix_.AsVector().size();
+            sparseMatrix.upper_matrix_ = this->upper_matrix_.AsVector().data(); 
+            sparseMatrix.upper_matrix_size_ = this->upper_matrix_.AsVector().size(); 
             denseMatrix.b_ = b.AsVector().data(); 
             denseMatrix.x_ = x.AsVector().data();
             denseMatrix.n_grids_ = b.size(); //number of grids
