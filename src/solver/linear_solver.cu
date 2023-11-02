dbc23d09b2b60642cbe568b4eebfca801117f720
Qina Tan <qina.tan@hotmail.com>
2023-11-02 13:02:17 -0600

    uncomment

--- src/solver/linear_solver.cu
+++ src/solver/linear_solver.cu
@@ -32,7 +32,7 @@ __global__ void SolveKernel(SolveDevice* device)
     std::pair<size_t, size_t>* Lij_yj = device->Lij_yj_;
     std::pair<size_t, size_t>* nUij_Uii = device->nUij_Uii_;
     std::pair<size_t, size_t>* Uij_xj = device->Uij_xj_;
-    //parallize grid cell
+    parallize grid cell
    if (tid < n_grids)
    { 
         size_t b_column_index = 0;
@@ -45,40 +45,36 @@ __global__ void SolveKernel(SolveDevice* device)
        
         for (size_t j = 0; j < device->nLij_Lii_size_; ++j)
         {
-            //auto& nLij_Lii_element = nLij_Lii[j]; 
-            //y[y_column_index * n_grids + tid] = b[b_column_index++ * n_grids + tid]; 
-            // for (size_t i = 0; i < nLij_Lii_element.first; ++i)
-            // {
-            //     size_t lower_matrix_index = Lij_yj[Lij_yj_index].first + tid;
-            //     size_t y_index = Lij_yj[Lij_yj_index].second * n_grids + tid; 
-            //     y[y_column_index * n_grids + tid] -= lower_matrix[lower_matrix_index] * y[y_index];
-            //     ++Lij_yj_index;  
-            // }
-            // y[y_column_index++ * n_grids + tid] /= lower_matrix[nLij_Lii_element.second + tid]; 
-                printf("y_column_index: %s\n", y_column_index); 
-                printf("vector position: %\n", y_column_index * n_grids + tid); 
-                y_column_index++; 
-            
+            auto& nLij_Lii_element = nLij_Lii[j]; 
+            y[y_column_index * n_grids + tid] = b[b_column_index++ * n_grids + tid]; 
+            for (size_t i = 0; i < nLij_Lii_element.first; ++i)
+            {
+                size_t lower_matrix_index = Lij_yj[Lij_yj_index].first + tid;
+                size_t y_index = Lij_yj[Lij_yj_index].second * n_grids + tid; 
+                y[y_column_index * n_grids + tid] -= lower_matrix[lower_matrix_index] * y[y_index];
+                ++Lij_yj_index;  
+            }
+            y[y_column_index++ * n_grids + tid] /= lower_matrix[nLij_Lii_element.second + tid]; 
         }
         
-        // for (size_t k = 0; k < device->nUij_Uii_size_; ++k)
-        // {   
-        //     auto& nUij_Uii_element = nUij_Uii[k]; 
+        for (size_t k = 0; k < device->nUij_Uii_size_; ++k)
+        {   
+            auto& nUij_Uii_element = nUij_Uii[k]; 
         
-        //     for (size_t i = 0; i < nUij_Uii_element.first; ++i)
-        //     {
-        //         size_t upper_matrix_index = Uij_xj[Uij_xj_index].first + tid;
-        //         size_t x_index = Uij_xj[Uij_xj_index].second * n_grids + tid;
-        //         x[x_column_backward_index * n_grids + tid] -= upper_matrix[upper_matrix_index] * x[x_index];
-        //         ++Uij_xj_index;
-        //     }
-        //     x[x_column_backward_index * n_grids + tid] /= upper_matrix[nUij_Uii_element.second + tid];
+            for (size_t i = 0; i < nUij_Uii_element.first; ++i)
+            {
+                size_t upper_matrix_index = Uij_xj[Uij_xj_index].first + tid;
+                size_t x_index = Uij_xj[Uij_xj_index].second * n_grids + tid;
+                x[x_column_backward_index * n_grids + tid] -= upper_matrix[upper_matrix_index] * x[x_index];
+                ++Uij_xj_index;
+            }
+            x[x_column_backward_index * n_grids + tid] /= upper_matrix[nUij_Uii_element.second + tid];
             
-        //     if (x_column_backward_index != 0)
-        //     {
-        //         --x_column_backward_index;
-        //     }
-        // }
+            if (x_column_backward_index != 0)
+            {
+                --x_column_backward_index;
+            }
+        }
     }
 }
     void SolveKernelDriver(CudaLinearSolverParam& linearSolver,CudaSparseMatrixParam& sparseMatrix, CudaMatrixParam& denseMatrix)
@@ -107,25 +103,25 @@ __global__ void SolveKernel(SolveDevice* device)
     cudaMalloc(&device, sizeof(SolveDevice));
 
     //transfer memory from host to device
-    // cudaMemcpy(d_nLij_Lii, linearSolver.nLij_Lii_, sizeof(std::pair<size_t, size_t>)* linearSolver.nLij_Lii_size_,cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_Lij_yj, linearSolver.Lij_yj_, sizeof(std::pair<size_t, size_t>)* linearSolver.Lij_yj_size_,cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_nUij_Uii, linearSolver.nUij_Uii_, sizeof(std::pair<size_t, size_t>)* linearSolver.nUij_Uii_size_,cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_Uij_xj, linearSolver.Uij_xj_, sizeof(std::pair<size_t, size_t>)* linearSolver.Uij_xj_size_, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_nLij_Lii, linearSolver.nLij_Lii_, sizeof(std::pair<size_t, size_t>)* linearSolver.nLij_Lii_size_,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Lij_yj, linearSolver.Lij_yj_, sizeof(std::pair<size_t, size_t>)* linearSolver.Lij_yj_size_,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_nUij_Uii, linearSolver.nUij_Uii_, sizeof(std::pair<size_t, size_t>)* linearSolver.nUij_Uii_size_,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Uij_xj, linearSolver.Uij_xj_, sizeof(std::pair<size_t, size_t>)* linearSolver.Uij_xj_size_, cudaMemcpyHostToDevice);
     
-    // cudaMemcpy(d_lower_matrix, sparseMatrix.lower_matrix_, sizeof(double)*sparseMatrix.lower_matrix_size_, cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_upper_matrix, sparseMatrix.upper_matrix_, sizeof(double)*sparseMatrix.upper_matrix_size_, cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_b, denseMatrix.b_, sizeof(double)* denseMatrix.b_size_, cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_x, denseMatrix.x_, sizeof(double)* denseMatrix.x_size_, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_lower_matrix, sparseMatrix.lower_matrix_, sizeof(double)*sparseMatrix.lower_matrix_size_, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_upper_matrix, sparseMatrix.upper_matrix_, sizeof(double)*sparseMatrix.upper_matrix_size_, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, denseMatrix.b_, sizeof(double)* denseMatrix.b_size_, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, denseMatrix.x_, sizeof(double)* denseMatrix.x_size_, cudaMemcpyHostToDevice);
     
-    // cudaMemcpy(&(device->nLij_Lii_), &d_nLij_Lii, sizeof(std::pair<size_t, size_t>*),cudaMemcpyHostToDevice);
-    // cudaMemcpy(&(device->Lij_yj_), &d_Lij_yj, sizeof(std::pair<size_t, size_t>*), cudaMemcpyHostToDevice);
-    // cudaMemcpy(&(device->nUij_Uii_), &d_nUij_Uii, sizeof(std::pair<size_t, size_t>*), cudaMemcpyHostToDevice);
-    // cudaMemcpy(&(device->Uij_xj_), &d_Uij_xj, sizeof(std::pair<size_t, size_t>*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(device->nLij_Lii_), &d_nLij_Lii, sizeof(std::pair<size_t, size_t>*),cudaMemcpyHostToDevice);
+    cudaMemcpy(&(device->Lij_yj_), &d_Lij_yj, sizeof(std::pair<size_t, size_t>*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(device->nUij_Uii_), &d_nUij_Uii, sizeof(std::pair<size_t, size_t>*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(device->Uij_xj_), &d_Uij_xj, sizeof(std::pair<size_t, size_t>*), cudaMemcpyHostToDevice);
     
-    // cudaMemcpy(&(device->lower_matrix_), &d_lower_matrix, sizeof(double*), cudaMemcpyHostToDevice);
-    // cudaMemcpy(&(device->upper_matrix_), &d_upper_matrix, sizeof(double*), cudaMemcpyHostToDevice);
-    // cudaMemcpy(&(device->b_), &d_b, sizeof(double*), cudaMemcpyHostToDevice); 
-    // cudaMemcpy(&(device->x_),&d_x, sizeof(double*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(device->lower_matrix_), &d_lower_matrix, sizeof(double*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(device->upper_matrix_), &d_upper_matrix, sizeof(double*), cudaMemcpyHostToDevice);
+    cudaMemcpy(&(device->b_), &d_b, sizeof(double*), cudaMemcpyHostToDevice); 
+    cudaMemcpy(&(device->x_),&d_x, sizeof(double*), cudaMemcpyHostToDevice);
     
     device->n_grids_= denseMatrix.n_grids_;
     device->b_column_counts_ = denseMatrix.b_column_counts_; 
@@ -136,7 +132,7 @@ __global__ void SolveKernel(SolveDevice* device)
     size_t num_block = (denseMatrix.n_grids_ + BLOCK_SIZE - 1) / BLOCK_SIZE;
     SolveKernel<<<num_block, BLOCK_SIZE>>>(device);
     cudaDeviceSynchronize();
-    // cudaMemcpy(denseMatrix.x_, d_x, sizeof(double)* denseMatrix.x_size_, cudaMemcpyDeviceToHost);
+    cudaMemcpy(denseMatrix.x_, d_x, sizeof(double)* denseMatrix.x_size_, cudaMemcpyDeviceToHost);
 
     //clean up 
     cudaFree(d_nLij_Lii);
