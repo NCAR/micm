0e03af4ea8ec40943315a982bd8a012ecb189275
Qina Tan <qina.tan@hotmail.com>
2023-11-03 12:25:07 -0600

    test with LuDecomposition class

--- include/micm/solver/cuda_linear_solver.hpp
+++ include/micm/solver/cuda_linear_solver.hpp
@@ -26,28 +26,27 @@ namespace micm{
             CudaLinearSolverParam linearSolver;
             CudaSparseMatrixParam sparseMatrix; 
             CudaMatrixParam denseMatrix;
-            // linearSolver.nLij_Lii_ = this->nLij_Lii_.data(); 
-            // linearSolver.nLij_Lii_size_ = this->nLij_Lii_.size(); 
-            // linearSolver.Lij_yj_= this->Lij_yj_.data();
-            // linearSolver.Lij_yj_size_ = this->Lij_yj_.size();
-            // linearSolver.nUij_Uii_ = this->nUij_Uii_.data();
-            // linearSolver.nUij_Uii_size_ = this->nUij_Uii_.size();
-            // linearSolver.Uij_xj_ = this->Uij_xj_.data(); 
-            // linearSolver.Uij_xj_size_ = this->Uij_xj_.size(); 
-            // sparseMatrix.lower_matrix_ = this->lower_matrix_.AsVector().data(); 
-            // sparseMatrix.lower_matrix_size_ = this->lower_matrix_.AsVector().size();
-            // sparseMatrix.upper_matrix_ = this->upper_matrix_.AsVector().data(); 
-            // sparseMatrix.upper_matrix_size_ = this->upper_matrix_.AsVector().size(); 
-            // denseMatrix.b_ = b.AsVector().data(); 
-            // denseMatrix.x_ = x.AsVector().data();
-            // denseMatrix.b_size_= b.AsVector().size();
-            // denseMatrix.x_size_= x.AsVector().size();
-            // denseMatrix.n_grids_ = b.size(); //number of grids
-            // denseMatrix.b_column_counts_ = b[0].size(); 
-            // denseMatrix.x_column_counts_= x[0].size();
-            // printf ("nLij_Lii size: %s\n", linearSolver.nLij_Lii_size_);
+            linearSolver.nLij_Lii_ = this->nLij_Lii_.data(); 
+            linearSolver.nLij_Lii_size_ = this->nLij_Lii_.size(); 
+            linearSolver.Lij_yj_= this->Lij_yj_.data();
+            linearSolver.Lij_yj_size_ = this->Lij_yj_.size();
+            linearSolver.nUij_Uii_ = this->nUij_Uii_.data();
+            linearSolver.nUij_Uii_size_ = this->nUij_Uii_.size();
+            linearSolver.Uij_xj_ = this->Uij_xj_.data(); 
+            linearSolver.Uij_xj_size_ = this->Uij_xj_.size(); 
+            sparseMatrix.lower_matrix_ = this->lower_matrix_.AsVector().data(); 
+            sparseMatrix.lower_matrix_size_ = this->lower_matrix_.AsVector().size();
+            sparseMatrix.upper_matrix_ = this->upper_matrix_.AsVector().data(); 
+            sparseMatrix.upper_matrix_size_ = this->upper_matrix_.AsVector().size(); 
+            denseMatrix.b_ = b.AsVector().data(); 
+            denseMatrix.x_ = x.AsVector().data();
+            denseMatrix.b_size_= b.AsVector().size();
+            denseMatrix.x_size_= x.AsVector().size();
+            denseMatrix.n_grids_ = b.size(); //number of grids
+            denseMatrix.b_column_counts_ = b[0].size(); 
+            denseMatrix.x_column_counts_= x[0].size();
             //calling kernel driver
-           // micm::cuda::SolveKernelDriver(linearSolver, sparseMatrix, denseMatrix);
+            micm::cuda::SolveKernelDriver(linearSolver, sparseMatrix, denseMatrix);
         };
     };
 }//end micm
