e634ac7aa313c4ae83a302476cf2b283f918bcf4
David Fillmore <fillmore@ucar.edu>
2023-11-12 09:18:31 -0700

    Added initial conditions.

--- test/surface_rxn/test_surface_rxn.cpp
+++ test/surface_rxn/test_surface_rxn.cpp
@@ -8,19 +8,18 @@
 
 int main(const int argc, const char* argv[])
 {
+  // parameters
   const double DENSITY_stuff      = 1000.0;  // [kg m-3]
   const double DENSITY_more_stuff = 1000.0;  // [kg m-3]
   const double MW_stuff           = 0.5;     // [kg mol-1]
   const double MW_more_stuff      = 0.2;     // [kg mol-1]
   const double MW_foo             = 0.04607; // [kg mol-1]
   const double Dg_foo             = 0.95e-5; // diffusion coeff [m2 s-1]
-  const double sp_number          = 1.3e6;   // single-particle number concentration [# m-3]
   const double rxn_gamma          = 2.0e-2;  // [unitless]
   const double bar_yield          = 1.0;     // [unitless]
   const double baz_yield          = 0.4;     // [unitless]
   const double mode_GMD           = 1.0e-6;  // mode geometric mean diameter [m]
   const double mode_GSD           = 0.1;     // mode geometric standard deviation [unitless]
-
 
   // environment
   const double temperature = 272.5;  // temperature (K)
@@ -31,10 +30,13 @@ int main(const int argc, const char* argv[])
   const double conc_stuff = 2.0e-3;
   const double conc_more_stuff = 3.0e-3;
 
+  // effective radius
   double radius = mode_GMD / 2.0 * exp(5.0 * log(mode_GSD) * log(mode_GSD) / 2.0);
 
+  // particle number concentration [# m-3]
   double number_conc = 6.0 / (std::numbers::pi * pow(mode_GMD, 3.0)
-    * exp(9.0/2.0 * log(mode_GSD) * log(mode_GSD) ));
-
+    * exp(9.0/2.0 * log(mode_GSD) * log(mode_GSD) ))
+    * (conc_stuff / DENSITY_stuff + conc_more_stuff / DENSITY_more_stuff);
+  
   return 0;
 }
