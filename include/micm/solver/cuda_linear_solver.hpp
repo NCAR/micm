cad92089cd11e87ea2d75791d9e1b212d17043ed
qinatan <qina.tan@hotmail.com>
2023-11-13 13:44:49 -0800

    modified solve function

--- include/micm/solver/cuda_linear_solver.hpp
+++ include/micm/solver/cuda_linear_solver.hpp
@@ -22,9 +22,10 @@ namespace micm{
         CudaLinearSolver(const SparseMatrixPolicy<T>& matrix, T initial_value, const std::function<LuDecompositionPolicy(const SparseMatrixPolicy<T>&)> create_lu_decomp)
         : linearSolver(matrix, initial_value, create_lu_decomp); 
        
-        template<template<class> class MatrixPolicy> 
+        template<template<class> class MatrixPolicy > 
         requires(VectorizableDense<MatrixPolicy<T>> || VectorizableSparse<SparseMatrixPolicy<T>>)
-        std::chrono::nanoseconds Solve(const MatrixPolicy<T>&b, MatrixPolicy<T>& x)
+        std::chrono::nanoseconds Solve(const MatrixPolicy<T>&b, MatrixPolicy<T>& x, SparseMatrixPolicy<T>& lower_matrix,
+        SparseMatrixPolicy<T>& upper_matrix)
         {
             CudaLinearSolverParam linearSolver;
             CudaSparseMatrixParam sparseMatrix; 
@@ -37,10 +38,10 @@ namespace micm{
             linearSolver.nUij_Uii_size_ = this->nUij_Uii_.size();
             linearSolver.Uij_xj_ = this->Uij_xj_.data(); 
             linearSolver.Uij_xj_size_ = this->Uij_xj_.size(); 
-            sparseMatrix.lower_matrix_ = this->lower_matrix_.AsVector().data(); 
-            sparseMatrix.lower_matrix_size_ = this->lower_matrix_.AsVector().size();
-            sparseMatrix.upper_matrix_ = this->upper_matrix_.AsVector().data(); 
-            sparseMatrix.upper_matrix_size_ = this->upper_matrix_.AsVector().size(); 
+            sparseMatrix.lower_matrix_ = lower_matrix.AsVector().data(); 
+            sparseMatrix.lower_matrix_size_ = lower_matrix.AsVector().size();
+            sparseMatrix.upper_matrix_ = upper_matrix.AsVector().data(); 
+            sparseMatrix.upper_matrix_size_ = upper_matrix.AsVector().size(); 
             denseMatrix.b_ = b.AsVector().data(); 
             denseMatrix.x_ = x.AsVector().data();
             denseMatrix.b_size_= b.AsVector().size();
