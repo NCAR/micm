802ad4f01e8954282ebc1407bcacbb03d8948bf3
Kyle Shores <kyle.shores44@gmail.com>
2023-11-15 13:36:24 -0600

    saving thing

--- test/examples/test_TS1.cpp
+++ test/examples/test_TS1.cpp
@@ -27,9 +27,10 @@ TEST(Examples, TS1)
                                    micm::RosenbrockSolverParameters::three_stage_rosenbrock_parameters() };
   micm::State state = solver.GetState();
 
-  const double temperature = 287.45;                             // K
+  const double temperature = 300;                             // K
   const double pressure = 101319.9;                              // Pa
-  const double air_density = pressure / (8.3145 * temperature);  // mol m-3
+  // const double air_density = pressure / (8.3145 * temperature);  // mol m-3
+  const double air_density = 44.48 * 6.02e+17;
 
   state.conditions_[0].temperature_ = temperature;
   state.conditions_[0].pressure_ = pressure;
@@ -56,161 +57,164 @@ TEST(Examples, TS1)
     { "NH3", { 1.0e-9 * air_density } },    // mol m-3
     { "M", { air_density } }                // mol m-3
   };
-  state.SetConcentrations(initial_concentrations);
-
-  // std::cout << "custom rate parameters" << std::endl;
-  // for(auto &rc : state.custom_rate_parameter_map_)
-  //   std::cout << "    { \"" << rc.first << "\", { 1.0e-8 } }, // s-1" << std::endl;
+  // state.SetConcentrations(initial_concentrations);
+  state.variables_ = 1e-6;
 
   // TODO: Replace with CAM-Chem rate constants when Derecho is back up
   std::unordered_map<std::string, std::vector<double>> custom_rate_constants = {
-    { "PHOTO.jacet", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jalknit->,jch3ooh", { 1.0e-8 } },      // s-1
-    { "PHOTO.jalkooh->,jch3ooh", { 1.0e-8 } },      // s-1
-    { "PHOTO.jbenzooh->,jch3ooh", { 1.0e-8 } },     // s-1
-    { "PHOTO.jbepomuc->,.10*jno2", { 1.0e-8 } },    // s-1
-    { "PHOTO.jbigald->,0.2*jno2", { 1.0e-8 } },     // s-1
-    { "PHOTO.jbigald1->,.14*jno2", { 1.0e-8 } },    // s-1
-    { "PHOTO.jbigald2->,.20*jno2", { 1.0e-8 } },    // s-1
-    { "PHOTO.jbigald3->,.20*jno2", { 1.0e-8 } },    // s-1
-    { "PHOTO.jbigald4->,.006*jno2", { 1.0e-8 } },   // s-1
-    { "PHOTO.jbrcl", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jbro", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jbrono2_a", { 1.0e-8 } },              // s-1
-    { "PHOTO.jbrono2_b", { 1.0e-8 } },              // s-1
-    { "PHOTO.jbzooh->,jch3ooh", { 1.0e-8 } },       // s-1
-    { "PHOTO.jc2h5ooh->,jch3ooh", { 1.0e-8 } },     // s-1
-    { "PHOTO.jc3h7ooh->,jch3ooh", { 1.0e-8 } },     // s-1
-    { "PHOTO.jc6h5ooh->,jch3ooh", { 1.0e-8 } },     // s-1
-    { "PHOTO.jccl4", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jcf2cl2", { 1.0e-8 } },                // s-1
-    { "PHOTO.jcf2clbr", { 1.0e-8 } },               // s-1
-    { "PHOTO.jcf3br", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jcfc113", { 1.0e-8 } },                // s-1
-    { "PHOTO.jcfc114", { 1.0e-8 } },                // s-1
-    { "PHOTO.jcfc115", { 1.0e-8 } },                // s-1
-    { "PHOTO.jcfcl3", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jch2br2", { 1.0e-8 } },                // s-1
-    { "PHOTO.jch2o_a", { 1.0e-8 } },                // s-1
-    { "PHOTO.jch2o_b", { 1.0e-8 } },                // s-1
-    { "PHOTO.jch3br", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jch3ccl3", { 1.0e-8 } },               // s-1
-    { "PHOTO.jch3cho", { 1.0e-8 } },                // s-1
-    { "PHOTO.jch3cl", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jch3co3h->,0.28*jh2o2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jch3ooh", { 1.0e-8 } },                // s-1
-    { "PHOTO.jch4_a", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jch4_b", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jchbr3", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jcl2", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jcl2o2", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jclo", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jclono2_a", { 1.0e-8 } },              // s-1
-    { "PHOTO.jclono2_b", { 1.0e-8 } },              // s-1
-    { "PHOTO.jco2", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jcof2", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jcofcl", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jeooh->,jch3ooh", { 1.0e-8 } },        // s-1
-    { "PHOTO.jglyald", { 1.0e-8 } },                // s-1
-    { "PHOTO.jglyoxal->,jmgly", { 1.0e-8 } },       // s-1
-    { "PHOTO.jh2402", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jh2o2", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jh2o_a", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jh2o_b", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jh2o_c", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jh2so4", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jhbr", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jhcfc141b", { 1.0e-8 } },              // s-1
-    { "PHOTO.jhcfc142b", { 1.0e-8 } },              // s-1
-    { "PHOTO.jhcfc22", { 1.0e-8 } },                // s-1
-    { "PHOTO.jhcl", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jhf", { 1.0e-8 } },                    // s-1
-    { "PHOTO.jhno3", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jho2no2_a", { 1.0e-8 } },              // s-1
-    { "PHOTO.jho2no2_b", { 1.0e-8 } },              // s-1
-    { "PHOTO.jhobr", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jhocl", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jhonitr->,jch2o_a", { 1.0e-8 } },      // s-1
-    { "PHOTO.jhpald->,.006*jno2", { 1.0e-8 } },     // s-1
-    { "PHOTO.jhyac", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jisopnooh->,jch3ooh", { 1.0e-8 } },    // s-1
-    { "PHOTO.jisopooh->,jch3ooh", { 1.0e-8 } },     // s-1
-    { "PHOTO.jmacr_a", { 1.0e-8 } },                // s-1
-    { "PHOTO.jmacr_b", { 1.0e-8 } },                // s-1
-    { "PHOTO.jmek->,jacet", { 1.0e-8 } },           // s-1
-    { "PHOTO.jmekooh->,jch3ooh", { 1.0e-8 } },      // s-1
-    { "PHOTO.jmgly", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jmpan->,jpan", { 1.0e-8 } },           // s-1
-    { "PHOTO.jmvk", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jn2o", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jn2o5_a", { 1.0e-8 } },                // s-1
-    { "PHOTO.jn2o5_b", { 1.0e-8 } },                // s-1
-    { "PHOTO.jnc4cho->,jch2o_a", { 1.0e-8 } },      // s-1
-    { "PHOTO.jno2", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jno3_a", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jno3_b", { 1.0e-8 } },                 // s-1
-    { "PHOTO.jno=userdefined,", { 1.0e-8 } },       // s-1
-    { "PHOTO.jnoa->,jch2o_a", { 1.0e-8 } },         // s-1
-    { "PHOTO.jnterpooh->,jch3ooh", { 1.0e-8 } },    // s-1
-    { "PHOTO.jo2_a=userdefined,", { 1.0e-8 } },     // s-1
-    { "PHOTO.jo2_b=userdefined,", { 1.0e-8 } },     // s-1
-    { "PHOTO.jo3_a", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jo3_b", { 1.0e-8 } },                  // s-1
-    { "PHOTO.joclo", { 1.0e-8 } },                  // s-1
-    { "PHOTO.jocs", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jonitr->,jch3cho", { 1.0e-8 } },       // s-1
-    { "PHOTO.jpan", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jphenooh->,jch3ooh", { 1.0e-8 } },     // s-1
-    { "PHOTO.jpooh->,jch3ooh", { 1.0e-8 } },        // s-1
-    { "PHOTO.jrooh->,jch3ooh", { 1.0e-8 } },        // s-1
-    { "PHOTO.jsf6", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jso", { 1.0e-8 } },                    // s-1
-    { "PHOTO.jso2", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jso3", { 1.0e-8 } },                   // s-1
-    { "PHOTO.jsoa1_a1->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa1_a2->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa2_a1->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa2_a2->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa3_a1->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa3_a2->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa4_a1->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa4_a2->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa5_a1->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jsoa5_a2->,.0004*jno2", { 1.0e-8 } },  // s-1
-    { "PHOTO.jtepomuc->,.10*jno2", { 1.0e-8 } },    // s-1
-    { "PHOTO.jterp2ooh->,jch3ooh", { 1.0e-8 } },    // s-1
-    { "PHOTO.jterpnit->,jch3ooh", { 1.0e-8 } },     // s-1
-    { "PHOTO.jterpooh->,jch3ooh", { 1.0e-8 } },     // s-1
-    { "PHOTO.jterprd1->,jch3cho", { 1.0e-8 } },     // s-1
-    { "PHOTO.jterprd2->,jch3cho", { 1.0e-8 } },     // s-1
-    { "PHOTO.jtolooh->,jch3ooh", { 1.0e-8 } },      // s-1
-    { "PHOTO.jxooh->,jch3ooh", { 1.0e-8 } },        // s-1
-    { "PHOTO.jxylenooh->,jch3ooh", { 1.0e-8 } },    // s-1
-    { "PHOTO.jxylolooh->,jch3ooh", { 1.0e-8 } },    // s-1
-    { "USER.het1", { 1.0e-8 } },                    // s-1
-    { "USER.het2", { 1.0e-8 } },                    // s-1
-    { "USER.het3", { 1.0e-8 } },                    // s-1
-    { "USER.het4", { 1.0e-8 } },                    // s-1
-    { "USER.het5", { 1.0e-8 } },                    // s-1
-    { "USER.het6", { 1.0e-8 } },                    // s-1
-    { "USER.het7", { 1.0e-8 } },                    // s-1
-    { "USER.het8", { 1.0e-8 } },                    // s-1
-    { "USER.het9", { 1.0e-8 } },                    // s-1
-    { "USER.het10", { 1.0e-8 } },                   // s-1
-    { "USER.het11", { 1.0e-8 } },                   // s-1
-    { "USER.het12", { 1.0e-8 } },                   // s-1
-    { "USER.het13", { 1.0e-8 } },                   // s-1
-    { "USER.het14", { 1.0e-8 } },                   // s-1
-    { "USER.het15", { 1.0e-8 } },                   // s-1
-    { "USER.het16", { 1.0e-8 } },                   // s-1
-    { "USER.het17", { 1.0e-8 } },                   // s-1
-    { "USER.k_co_oh_jpl19", { 1.0e-8 } }            // s-1
+    { "PHOTO.jacet", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jalknit->,jch3ooh", { 2.0e-8 } },      // s-1
+    { "PHOTO.jalkooh->,jch3ooh", { 2.0e-8 } },      // s-1
+    { "PHOTO.jbenzooh->,jch3ooh", { 2.0e-8 } },     // s-1
+    { "PHOTO.jbepomuc->,.10*jno2", { 2.0e-8 } },    // s-1
+    { "PHOTO.jbigald->,0.2*jno2", { 2.0e-8 } },     // s-1
+    { "PHOTO.jbigald1->,.14*jno2", { 2.0e-8 } },    // s-1
+    { "PHOTO.jbigald2->,.20*jno2", { 2.0e-8 } },    // s-1
+    { "PHOTO.jbigald3->,.20*jno2", { 2.0e-8 } },    // s-1
+    { "PHOTO.jbigald4->,.006*jno2", { 2.0e-8 } },   // s-1
+    { "PHOTO.jbrcl", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jbro", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jbrono2_a", { 2.0e-8 } },              // s-1
+    { "PHOTO.jbrono2_b", { 2.0e-8 } },              // s-1
+    { "PHOTO.jbzooh->,jch3ooh", { 2.0e-8 } },       // s-1
+    { "PHOTO.jc2h5ooh->,jch3ooh", { 2.0e-8 } },     // s-1
+    { "PHOTO.jc3h7ooh->,jch3ooh", { 2.0e-8 } },     // s-1
+    { "PHOTO.jc6h5ooh->,jch3ooh", { 2.0e-8 } },     // s-1
+    { "PHOTO.jccl4", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jcf2cl2", { 2.0e-8 } },                // s-1
+    { "PHOTO.jcf2clbr", { 2.0e-8 } },               // s-1
+    { "PHOTO.jcf3br", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jcfc113", { 2.0e-8 } },                // s-1
+    { "PHOTO.jcfc114", { 2.0e-8 } },                // s-1
+    { "PHOTO.jcfc115", { 2.0e-8 } },                // s-1
+    { "PHOTO.jcfcl3", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jch2br2", { 2.0e-8 } },                // s-1
+    { "PHOTO.jch2o_a", { 2.0e-8 } },                // s-1
+    { "PHOTO.jch2o_b", { 2.0e-8 } },                // s-1
+    { "PHOTO.jch3br", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jch3ccl3", { 2.0e-8 } },               // s-1
+    { "PHOTO.jch3cho", { 2.0e-8 } },                // s-1
+    { "PHOTO.jch3cl", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jch3co3h->,0.28*jh2o2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jch3ooh", { 2.0e-8 } },                // s-1
+    { "PHOTO.jch4_a", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jch4_b", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jchbr3", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jcl2", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jcl2o2", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jclo", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jclono2_a", { 2.0e-8 } },              // s-1
+    { "PHOTO.jclono2_b", { 2.0e-8 } },              // s-1
+    { "PHOTO.jco2", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jcof2", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jcofcl", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jeooh->,jch3ooh", { 2.0e-8 } },        // s-1
+    { "PHOTO.jglyald", { 2.0e-8 } },                // s-1
+    { "PHOTO.jglyoxal->,jmgly", { 2.0e-8 } },       // s-1
+    { "PHOTO.jh2402", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jh2o2", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jh2o_a", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jh2o_b", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jh2o_c", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jh2so4", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jhbr", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jhcfc141b", { 2.0e-8 } },              // s-1
+    { "PHOTO.jhcfc142b", { 2.0e-8 } },              // s-1
+    { "PHOTO.jhcfc22", { 2.0e-8 } },                // s-1
+    { "PHOTO.jhcl", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jhf", { 2.0e-8 } },                    // s-1
+    { "PHOTO.jhno3", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jho2no2_a", { 2.0e-8 } },              // s-1
+    { "PHOTO.jho2no2_b", { 2.0e-8 } },              // s-1
+    { "PHOTO.jhobr", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jhocl", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jhonitr->,jch2o_a", { 2.0e-8 } },      // s-1
+    { "PHOTO.jhpald->,.006*jno2", { 2.0e-8 } },     // s-1
+    { "PHOTO.jhyac", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jisopnooh->,jch3ooh", { 2.0e-8 } },    // s-1
+    { "PHOTO.jisopooh->,jch3ooh", { 2.0e-8 } },     // s-1
+    { "PHOTO.jmacr_a", { 2.0e-8 } },                // s-1
+    { "PHOTO.jmacr_b", { 2.0e-8 } },                // s-1
+    { "PHOTO.jmek->,jacet", { 2.0e-8 } },           // s-1
+    { "PHOTO.jmekooh->,jch3ooh", { 2.0e-8 } },      // s-1
+    { "PHOTO.jmgly", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jmpan->,jpan", { 2.0e-8 } },           // s-1
+    { "PHOTO.jmvk", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jn2o", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jn2o5_a", { 2.0e-8 } },                // s-1
+    { "PHOTO.jn2o5_b", { 2.0e-8 } },                // s-1
+    { "PHOTO.jnc4cho->,jch2o_a", { 2.0e-8 } },      // s-1
+    { "PHOTO.jno2", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jno3_a", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jno3_b", { 2.0e-8 } },                 // s-1
+    { "PHOTO.jno=userdefined,", { 2.0e-8 } },       // s-1
+    { "PHOTO.jnoa->,jch2o_a", { 2.0e-8 } },         // s-1
+    { "PHOTO.jnterpooh->,jch3ooh", { 2.0e-8 } },    // s-1
+    { "PHOTO.jo2_a=userdefined,", { 2.0e-8 } },     // s-1
+    { "PHOTO.jo2_b=userdefined,", { 2.0e-8 } },     // s-1
+    { "PHOTO.jo3_a", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jo3_b", { 2.0e-8 } },                  // s-1
+    { "PHOTO.joclo", { 2.0e-8 } },                  // s-1
+    { "PHOTO.jocs", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jonitr->,jch3cho", { 2.0e-8 } },       // s-1
+    { "PHOTO.jpan", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jphenooh->,jch3ooh", { 2.0e-8 } },     // s-1
+    { "PHOTO.jpooh->,jch3ooh", { 2.0e-8 } },        // s-1
+    { "PHOTO.jrooh->,jch3ooh", { 2.0e-8 } },        // s-1
+    { "PHOTO.jsf6", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jso", { 2.0e-8 } },                    // s-1
+    { "PHOTO.jso2", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jso3", { 2.0e-8 } },                   // s-1
+    { "PHOTO.jsoa1_a1->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa1_a2->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa2_a1->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa2_a2->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa3_a1->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa3_a2->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa4_a1->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa4_a2->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa5_a1->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jsoa5_a2->,.0004*jno2", { 2.0e-8 } },  // s-1
+    { "PHOTO.jtepomuc->,.10*jno2", { 2.0e-8 } },    // s-1
+    { "PHOTO.jterp2ooh->,jch3ooh", { 2.0e-8 } },    // s-1
+    { "PHOTO.jterpnit->,jch3ooh", { 2.0e-8 } },     // s-1
+    { "PHOTO.jterpooh->,jch3ooh", { 2.0e-8 } },     // s-1
+    { "PHOTO.jterprd1->,jch3cho", { 2.0e-8 } },     // s-1
+    { "PHOTO.jterprd2->,jch3cho", { 2.0e-8 } },     // s-1
+    { "PHOTO.jtolooh->,jch3ooh", { 2.0e-8 } },      // s-1
+    { "PHOTO.jxooh->,jch3ooh", { 2.0e-8 } },        // s-1
+    { "PHOTO.jxylenooh->,jch3ooh", { 2.0e-8 } },    // s-1
+    { "PHOTO.jxylolooh->,jch3ooh", { 2.0e-8 } },    // s-1
+    { "USER.het1", { 2.0e-8 } },                    // s-1
+    { "USER.het2", { 2.0e-8 } },                    // s-1
+    { "USER.het3", { 2.0e-8 } },                    // s-1
+    { "USER.het4", { 2.0e-8 } },                    // s-1
+    { "USER.het5", { 2.0e-8 } },                    // s-1
+    { "USER.het6", { 2.0e-8 } },                    // s-1
+    { "USER.het7", { 2.0e-8 } },                    // s-1
+    { "USER.het8", { 2.0e-8 } },                    // s-1
+    { "USER.het9", { 2.0e-8 } },                    // s-1
+    { "USER.het10", { 2.0e-8 } },                   // s-1
+    { "USER.het11", { 2.0e-8 } },                   // s-1
+    { "USER.het12", { 2.0e-8 } },                   // s-1
+    { "USER.het13", { 2.0e-8 } },                   // s-1
+    { "USER.het14", { 2.0e-8 } },                   // s-1
+    { "USER.het15", { 2.0e-8 } },                   // s-1
+    { "USER.het16", { 2.0e-8 } },                   // s-1
+    { "USER.het17", { 2.0e-8 } },                   // s-1
+    { "USER.k_co_oh_jpl19", { 2.0e-8 } }            // s-1
   };
   state.SetCustomRateParameters(custom_rate_constants);
 
   double time_step = 150.0;  // s
 
   auto result = solver.Solve(time_step, state);
+
+  state.variables_ = result.result_;
+
+  for(auto& pair : state.variable_map_) {
+    std::cout << pair.first << " : " << state.variables_[0][pair.second] << std::endl;
+  }
 
   EXPECT_EQ(result.state_, (micm::SolverState::Converged));
 }
