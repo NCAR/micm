30ee43d8a2839b7d2e230974193153df33982d96
Jiwon Gim <jiwongim@ucar.edu>
2024-04-24 19:45:10 -0600

    order include headers

--- src/solver/lu_decomposition.cu
+++ src/solver/lu_decomposition.cu
@@ -1,9 +1,11 @@
-// Copyright (C) 2023-2024 National Center for Atmospheric Research,
-//
-// SPDX-License-Identifier: Apache-2.0
+/* Copyright (C) 2023-2024 National Center for Atmospheric Research,
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <micm/util/cuda_param.hpp>
+
 #include <chrono>
 #include <iostream>
-#include <micm/util/cuda_param.hpp>
 
 namespace micm
 {
