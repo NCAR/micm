ba1ca519da61c149a62545bdfb2abe94ba05e7d8
Jian Sun <sunjian@ucar.edu>
2024-05-01 11:29:50 -0600

    update one more test and remove const qualifier

--- test/unit/util/test_cuda_sparse_matrix.cpp
+++ test/unit/util/test_cuda_sparse_matrix.cpp
@@ -145,9 +145,13 @@ TEST(CudaSparseMatrix, MoveAssignmentConstZeroMatrix)
                      .with_element(1, 1)
                      .initial_value(0.0);
 
-  const micm::CudaSparseMatrix<double, micm::SparseMatrixVectorOrdering<1>> matrix{ builder };
+  micm::CudaSparseMatrix<double, micm::SparseMatrixVectorOrdering<1>> matrix{ builder };
 
   auto oneMatrix = std::move(matrix);
+  if (matrix.AsDeviceParam().d_data_ != nullptr)
+  {
+    throw std::runtime_error("The 'd_data_' pointer of oneMatrix is not initialized to a null pointer in the move constructor.");
+  }
 
   EXPECT_EQ(2, oneMatrix.AsVector().size());
   for (auto& elem : oneMatrix.AsVector())
