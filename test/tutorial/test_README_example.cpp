63368c0ad056f84a2d60c66f7a405741e9dfe36f
Kyle Shores <kyle.shores44@gmail.com>
2024-05-24 13:34:57 -0500

    reverting readme example

--- test/tutorial/test_README_example.cpp
+++ test/tutorial/test_README_example.cpp
@@ -30,52 +30,21 @@ int main(const int argc, const char *argv[])
 
   std::vector<Process> reactions{ r1, r2 };
 
-#if 0
-  auto rosenbrock_solver = Solver::Create()
-                    .SetSystem(chemical_system)
-                    .SetReactions(reactions)
-                    .SetNumberOfGridCells(19)
-                    .Rosenbrock<MatrixPolicy, SparseMatrixPolicy>(RosenbrockSolverParameters::ThreeStageRosenbrockParameters());
+  RosenbrockSolver<> solver{ chemical_system, reactions, RosenbrockSolverParameters::ThreeStageRosenbrockParameters() };
 
-  auto backward_euler_solver = Solver::Create()
-                    .SetSystem(chemical_system)
-                    .SetReactions(reactions)
-                    .SetNumberOfGridCells(19)
-                    .BackwardEuler(BackwardEulerParameters::CamChemParameters());
+  State state = solver.GetState();
 
-  auto cuda_solver = Solver::Create()
-                    .SetSystem(chemical_system)
-                    .SetReactions(reactions)
-                    .SetNumberOfGridCells(19)
-                    .Rosenbrock<CudaDenseMatrixPolicy, CudaSparseMatrixPolicy>(RosenbrockSolverParameters::ThreeStageRosenbrockParameter());
-  
+  state.conditions_[0].temperature_ = 287.45;  // K
+  state.conditions_[0].pressure_ = 101319.9;   // Pa
+  state.SetConcentration(foo, 20.0);           // mol m-3
 
-  auto rosenbrock_state = rosenbrock_solver.GetState();
-  auto backward_euler_state = backward_euler_solver.GetState();
-  auto cuda_state = cuda_solver.GetState();
-
-  rosenbrock_state.conditions_[0].temperature_ = 287.45;  // K
-  rosenbrock_state.conditions_[0].pressure_ = 101319.9;   // Pa
-  rosenbrock_state.SetConcentration(foo, 20.0);           // mol m-3
-
-  backward_euler_state.conditions_[0].temperature_ = 287.45;  // K
-  backward_euler_state.conditions_[0].pressure_ = 101319.9;   // Pa
-  backward_euler_state.SetConcentration(foo, 20.0);           // mol m-3
-
-  cuda_state.conditions_[0].temperature_ = 287.45;  // K
-  cuda_state.conditions_[0].pressure_ = 101319.9;   // Pa
-  cuda_state.SetConcentration(foo, 20.0);           // mol m-3
-
-  cuda_state.PrintHeader();
+  state.PrintHeader();
   for (int i = 0; i < 10; ++i)
   {
-    cuda_state.CalculateRateConstants(); // calculate rate constants on the host
-    cuda_state.SyncToDevice();
-    auto result = cuda_solver.Solve(500.0, cuda_state); // solve on the device
-    cuda_state.variables_ = result.result_; // device to device copy with overloaded operator=
-    cuda_state.SyncToHost();
-    cuda_state.PrintState(i * 500);
+    auto result = solver.Solve(500.0, state);
+    state.variables_ = result.result_;
+    state.PrintState(i * 500);
   }
-#endif
+
   return 0;
 }
