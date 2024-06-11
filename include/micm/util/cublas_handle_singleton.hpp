f7bb59498d3a1534e2bab979f119848eb7064c7a
Jian Sun <sunjian@ucar.edu>
2024-06-11 11:50:39 -0600

    update the cublas singleton class

--- include/micm/util/cublas_handle_singleton.hpp
+++ include/micm/util/cublas_handle_singleton.hpp
@@ -1,94 +1,80 @@
 #include <memory>
 #include <mutex>
+#include <cstdlib>   // for getenv
+#include <string>    // for std::stoi
 #include <cublas_v2.h>
 #include <micm/util/cuda_util.cuh>
 
 namespace micm
 {
-    class CublasHandleSingleton {
-public:
-    // Delete copy constructor and assignment operator to prevent copies
-    CublasHandleSingleton(const CublasHandleSingleton&) = delete;
-    CublasHandleSingleton& operator=(const CublasHandleSingleton&) = delete;
+  class CublasHandleSingleton {
+    public:
+      // Delete copy constructor and assignment operator to prevent copies
+      CublasHandleSingleton(const CublasHandleSingleton&) = delete;
+      CublasHandleSingleton& operator=(const CublasHandleSingleton&) = delete;
 
-    // Static method to get the singleton instance
-    static CublasHandleSingleton& GetInstance() {
+      // Static method to get the singleton instance
+      static CublasHandleSingleton& GetInstance() {
         static CublasHandleSingleton instance;
         return instance;
-    }
+      }
 
-    // Method to get the cublas handle
-    cublasHandle_t & GetCublasHandle() {
-        return handle;
-    }
-
-private:
-    inline static cublasHandle_t handle;
-
-    // Private constructor
-    CublasHandleSingleton() {
-        // Initialize the cuBLAS handle
-        std::cout << "JS: ready to call constructor..." << std::endl;
-        cublasStatus_t status = cublasCreate(&handle);
-        if (status != CUBLAS_STATUS_SUCCESS) {
-            std::cerr << "CUBLAS initialization failed\n";
-            throw std::runtime_error("CUBLAS initialization failed");
+      // Get the static instance of CublasHandleSingleton class
+      static CublasHandleSingleton& GetInstance()
+      {
+        const char* number_of_openmp_threads = std::getenv("OMP_NUM_THREADS");
+        const char* number_of_cuda_devices = std::getenv("CUDA_VISIBLE_DEVICES");
+        if (number_of_openmp_threads != nullptr && number_of_cuda_devices != nullptr)
+        {
+            if (std::stoi(number_of_openmp_threads) > std::stoi(number_of_cuda_devices))
+            {
+            std::cout << "Mapping multiple OpenMP threads to the same GPU...\n";
+            std::lock_guard<std::mutex> lock(GetMutex()); // Lock the mutex so that only one thread will execute the following code at a time;
+                                                            // Unlock the mutex when the thread goes out of scope;
+                                                            // I think this is needed if multiple threads are mapped to the same GPU.
+            }
         }
-    }
+        else
+        {
+            throw std::runtime_error("Please set the environment variables OMP_NUM_THREADS and CUDA_VISIBLE_DEVICES.\n");
+        }
+        static CublasHandleSingleton instance;
+        return instance;
+      }
 
-    // Private destructor
-    ~CublasHandleSingleton() {
+      // Method to get the cuBLAS handle
+      cublasHandle_t& GetCublasHandle() {
+        return handle_;
+      }
+
+    private:
+      inline static cublasHandle_t handle_;
+
+      static std::mutex& GetMutex()
+      {
+        static std::mutex mutex;
+        return mutex;
+      }
+
+      // Private constructor to prevent instantiation
+      CublasHandleSingleton()
+      {
+        // Initialize the cuBLAS handle
+        if (!handle_)
+        {
+          CHECK_CUBLAS_ERROR(cublasCreate(&handle_), "CUBLAS initialization failed...");
+        }
+      }
+
+      // Private destructor
+      ~CublasHandleSingleton()
+      {
         // Destroy the cuBLAS handle
-        std::cout << "JS: ready to call destructor..." << std::endl;
-        cublasDestroy(handle);
-    }
-};
-
-
-//     class CublasHandleSingleton {
-//     public:
-//         // Get the static instance of CublasHandleSingleton class
-//         static CublasHandleSingleton& GetInstance() {
-//             std::lock_guard<std::mutex> lock(GetMutex()); // Lock the mutex
-//             std::cout << "JS: ready to call constructor..." << std::endl;
-//             static CublasHandleSingleton instance;
-//             return instance;
-//         }
-
-//         // Get the cublas handle as a shared pointer with custom deleter
-//         cublasHandle_t GetCublasHandle() {
-//             std::cout << "JS: GetCublasHandle is called" << std::endl;
-//             return handle_;
-//         }
-
-//     private:
-//         cublasHandle_t handle_;
-//         static std::mutex& GetMutex() {
-//             static std::mutex mutex;
-//             return mutex;
-//         }
-
-//         // Private constructor to prevent instantiation
-//         CublasHandleSingleton()
-//         {
-// //            if (!handle_)
-// //            {
-//                 CHECK_CUBLAS_ERROR(cublasCreate(&handle_), "CUBLAS initialization failed...");
-// //            }
-//         }
-        
-//         // Delete copy constructor and assignment operator
-//         CublasHandleSingleton(const CublasHandleSingleton&) = delete;
-//         CublasHandleSingleton& operator=(const CublasHandleSingleton&) = delete;
-
-//         // Destructor
-//         ~CublasHandleSingleton()
-//         {
-//             if (handle_)
-//             {
-//                 CHECK_CUBLAS_ERROR(cublasDestroy(handle_), "CUBLAS finalization failed...");
-//                 this->handle_ = NULL;
-//             }
-//         }
-//     };
-}
+        if (handle_)
+        {
+          CHECK_CUBLAS_ERROR(cublasDestroy(handle_), "CUBLAS finalization failed...");
+          this->handle_ = NULL;
+        }
+      }
+  }; // class CublasHandleSingleton
+} // namespace micm
