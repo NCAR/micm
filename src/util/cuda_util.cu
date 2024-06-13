81614c7e8bf5d355fca6f192762d6666a52bc805
Jian Sun <sunjian@ucar.edu>
2024-06-13 09:31:07 -0600

    use smart pointer to deallocate the cublas handle explicitly

--- src/util/cuda_util.cu
+++ src/util/cuda_util.cu
@@ -31,20 +31,38 @@ namespace micm
       }
     }
 
+    // Define a functor for the cublasHandle_t unique pointer deleter
+    struct CublasHandleDeleter {
+      void operator()(cublasHandle_t* handle) const {
+        if (handle != nullptr) {
+          CHECK_CUBLAS_ERROR(cublasDestroy(*handle), "CUBLAS finalization failed");
+          delete handle;
+        }
+      }
+    };
+
+    // Define the smart pointer type using the functor for the custom deleter
+    using CublasHandlePtr = std::unique_ptr<cublasHandle_t, CublasHandleDeleter>;
+
+    // Create a cublas handle and return a unique pointer to it
+    CublasHandlePtr CreateCublasHandle() {
+      cublasHandle_t* handle = new cublasHandle_t;
+      CHECK_CUBLAS_ERROR(cublasCreate(handle), "CUBLAS initialization failed...");
+      return CublasHandlePtr(handle, CublasHandleDeleter());
+    }
+  
     cublasHandle_t& GetCublasHandle()
     {
-      static std::map<int, cublasHandle_t> cublas_handles_map;
+      static std::map<int, CublasHandlePtr> cublas_handles_map;
       static std::mutex mutex;
       int device_id;
       CHECK_CUDA_ERROR(cudaGetDevice(&device_id), "Failed to get device ID...");
       std::lock_guard<std::mutex> lock(mutex); // lock the mutex and generate a new cublas handle below
       if (auto search = cublas_handles_map.find(device_id); search == cublas_handles_map.end())
       {
-        cublasHandle_t handle;
-        CHECK_CUBLAS_ERROR(cublasCreate(&handle), "CUBLAS initialization failed..."); // create the cublas handle
-        cublas_handles_map[device_id] = handle;                                       // save the cublas handle
+        cublas_handles_map[device_id] = std::move(CreateCublasHandle());
       }
-      return cublas_handles_map[device_id];
+      return *cublas_handles_map[device_id];
     }
   }  // namespace cuda
 }  // namespace micm
