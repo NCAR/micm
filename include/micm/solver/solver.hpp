62068456748d50d5e4d1a5302b083f6d08625485
Jian Sun <sunjian@ucar.edu>
2024-07-10 21:49:24 -0600

    more bug fixes

--- include/micm/solver/solver.hpp
+++ include/micm/solver/solver.hpp
@@ -50,12 +50,12 @@ namespace micm
     }
     Solver& operator=(Solver&& other)
     {
-      solver_ = std::swap(other.solver_);
+      std::swap(this->solver_,other.solver_);
       number_of_grid_cells_ = other.number_of_grid_cells_;
       number_of_species_ = other.number_of_species_;
       number_of_reactions_ = other.number_of_reactions_;
       state_parameters_ = other.state_parameters_;
-      processes_ = std::swap(other.processes_);
+      std::swap(this->solver_,other.processes_);
       return *this;
     }
 
