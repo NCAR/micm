b529224e0a8ac5e3addba7e217bbc5c590d5af55
Matt Dawson <mattdawson@ucar.edu>
2024-12-23 16:57:12 -0800

    allow in-place linear solve for Backward Euler

--- include/micm/solver/linear_solver_in_place.hpp
+++ include/micm/solver/linear_solver_in_place.hpp
@@ -12,7 +12,6 @@
 
 namespace micm
 {
-
   /// @brief A general-use block-diagonal sparse-matrix linear solver
   ///
   /// The sparsity pattern of each block in the block diagonal matrix is the same.
--- include/micm/solver/lu_decomposition.hpp
+++ include/micm/solver/lu_decomposition.hpp
@@ -5,9 +5,22 @@
 #include "lu_decomposition_doolittle.hpp"
 #include "lu_decomposition_mozart.hpp"
 #include "lu_decomposition_mozart_in_place.hpp"
+#include <micm/util/sparse_matrix.hpp>
+#include <micm/util/sparse_matrix_vector_ordering.hpp>
 
 namespace micm
 {
+
+  /// @brief Concept for in-place LU decomposition algorithms
+  template<class T, class SparseMatrixPolicy>
+  concept LuDecompositionInPlaceConcept = requires(T t)
+  {
+    { t.GetLUMatrix(SparseMatrixPolicy{}, 0.0) };
+    { t.Decompose(std::declval<SparseMatrixPolicy&>()) };
+  };
+  static_assert(LuDecompositionInPlaceConcept<LuDecompositionMozartInPlace, StandardSparseMatrix>, "LuDecompositionMozartInPlace does not meet the LuDecompositionInPlaceConcept requirements");
+  static_assert(LuDecompositionInPlaceConcept<LuDecompositionMozartInPlace, SparseMatrix<double, SparseMatrixVectorOrderingCompressedSparseRow<1>>>, "LuDecompositionMozartInPlace for vector matrices does not meet the LuDecompositionInPlaceConcept requirements");
+
   /// @brief Alias for the default LU decomposition algorithm
   using LuDecomposition = LuDecompositionDoolittle;
 
