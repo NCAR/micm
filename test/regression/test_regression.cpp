dc623c9d159ca916454c77ba57c91c3bff7c3bf0
Kyle Shores <kyle.shores44@gmail.com>
2025-04-11 09:29:30 -0500

    adding wall loss regression test

---
+++ test/regression/test_regression.cpp
@@ -0,0 +1,23 @@
+#include "regression_policy.hpp"
+
+#include <micm/solver/solver_builder.hpp>
+#include <micm/solver/rosenbrock.hpp>
+#include <micm/solver/rosenbrock_solver_parameters.hpp>
+#include <micm/solver/backward_euler_solver_parameters.hpp>
+
+#include <gtest/gtest.h>
+
+auto rosenbrock_2stage = micm::CpuSolverBuilder<micm::RosenbrockSolverParameters>(
+    micm::RosenbrockSolverParameters::TwoStageRosenbrockParameters());
+
+auto backward_euler = micm::CpuSolverBuilder<micm::BackwardEulerSolverParameters>(micm::BackwardEulerSolverParameters());
+
+TEST(RossenbrockRegression, FlowTube)
+{
+  test_flow_tube(rosenbrock_2stage, "expected_results/rosenbrock_wall_loss.csv");
+}
+
+TEST(BackwardEulerRegression, FlowTube)
+{
+  test_flow_tube(backward_euler, "expected_results/backward_euler_wall_loss.csv");
+}
