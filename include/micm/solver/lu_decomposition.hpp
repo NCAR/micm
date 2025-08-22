64703ca6b10ff290cae6f46b2528476a43476408
Jiwon Gim <jiwongim@ucar.edu>
2025-08-22 15:13:10 -0600

    test fixes

--- include/micm/solver/lu_decomposition.hpp
+++ include/micm/solver/lu_decomposition.hpp
@@ -16,12 +16,8 @@ namespace micm
   /// @brief Concept for in-place LU decomposition algorithms
   template<class T, class SparseMatrixPolicy>
   concept LuDecompositionInPlaceConcept = requires(T t) {
-    {
-      t.GetLUMatrix(SparseMatrixPolicy{}, 0.0, true)
-    };
-    {
-      t.Decompose(std::declval<SparseMatrixPolicy&>())
-    };
+    { t.GetLUMatrix(SparseMatrixPolicy{}, 0.0, true) };
+    { t.Decompose(std::declval<SparseMatrixPolicy&>()) };
   };
   static_assert(
       LuDecompositionInPlaceConcept<LuDecompositionMozartInPlace, StandardSparseMatrix>,
