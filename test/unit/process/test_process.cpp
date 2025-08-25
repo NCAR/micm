198308b572b96a25af1f91c98299e5ad2fe7f17f
Jiwon Gim <55209567+boulderdaze@users.noreply.github.com>
2025-08-25 12:10:37 -0600

    SurfaceRateConstant should throw when used with multiple reactants (#823)
    * add virtual to the member functions
    
    * create a dedicated rate constant directory for organizing
    
    * rename process types
    
    * replace process with chemcial reaction class
    
    * add phase transfer process
    
    * temp for test
    
    * add set solvent function
    
    * update the path
    
    * process test
    
    * replace process constructor with  chemical reaction builder
    
    * make the tests pass
    
    * update readme:
    
    * clang formatting
    
    * code cleanup
    
    * add headers
    
    * code cleanup
    
    * fix bugs
    
    * address code review from copilot
    
    * add missing headers
    
    * specify xcode version
    
    * address code review
    
    * revert back to SetPhase()
    
    * make it phase as pointer
    
    * make it ptr
    
    * fix tests
    
    * create an independent process error file
    
    * takes pointer to phase
    
    * comment todo
    
    * fix a test
    
    * fix cuda test
    
    * code cleanup, ready for review
    
    * surface reaction number of reactants checks
    
    * create rate constant directory
    
    * include test util file only once
    
    * tightens tolerances
    
    * address code review
    
    * add chemical reaction and phase transfer test
    
    * fix code comment
    
    * temp
    
    * address code review
    
    * fix the test
    
    * remove comments
    
    * test clang-format
    
    * fix clang format
    
    * fix clang format version issue
    
    * test fixes
    
    * fix bugs
    
    * revert back to original

--- test/unit/process/test_process.cpp
+++ test/unit/process/test_process.cpp
@@ -26,9 +26,21 @@ void testProcessUpdateState(const std::size_t number_of_grid_cells)
   SurfaceRateConstant rc2({ .label_ = "foo_surf", .species_ = foo });
   UserDefinedRateConstant rc3({ .label_ = "bar_user" });
 
-  Process r1 = ChemicalReactionBuilder().SetReactants({ foo, bar }).SetRateConstant(rc1).SetPhase(gas_phase).Build();
-  Process r2 = ChemicalReactionBuilder().SetRateConstant(rc2).SetPhase(gas_phase).Build();
-  Process r3 = ChemicalReactionBuilder().SetRateConstant(rc3).SetPhase(gas_phase).Build();
+  Process r1 = ChemicalReactionBuilder()
+                  .SetReactants({ foo, bar })
+                  .SetRateConstant(rc1)
+                  .SetPhase(gas_phase)
+                  .Build();
+  Process r2 = ChemicalReactionBuilder()
+                  .SetReactants({ foo })
+                  .SetRateConstant(rc2)
+                  .SetPhase(gas_phase)
+                  .Build();
+  Process r3 = ChemicalReactionBuilder()
+                  .SetReactants({ bar })
+                  .SetRateConstant(rc3)
+                  .SetPhase(gas_phase)
+                  .Build();
   std::vector<Process> processes = { r1, r2, r3 };
 
   std::vector<std::string> param_labels{};
@@ -62,19 +74,17 @@ void testProcessUpdateState(const std::size_t number_of_grid_cells)
     params[1] = get_double() * 1.0e5;
     params[2] = get_double() * 1.0e-2;
     state.custom_rate_parameters_[i_cell][state.custom_rate_parameter_map_["foo_surf.effective radius [m]"]] = params[0];
-    state.custom_rate_parameters_[i_cell]
-                                 [state.custom_rate_parameter_map_["foo_surf.particle number concentration [# m-3]"]] =
-        params[1];
+    state.custom_rate_parameters_[i_cell][state.custom_rate_parameter_map_["foo_surf.particle number concentration [# m-3]"]] = params[1];
     state.custom_rate_parameters_[i_cell][state.custom_rate_parameter_map_["bar_user"]] = params[2];
     std::vector<double>::const_iterator param_iter = params.begin();
-    expected_rate_constants[i_cell][0] =
-        rc1.Calculate(state.conditions_[i_cell], param_iter) * (state.conditions_[i_cell].air_density_ * 0.82);
+    expected_rate_constants[i_cell][0] = rc1.Calculate(state.conditions_[i_cell], param_iter) * (state.conditions_[i_cell].air_density_ * 0.82);
     param_iter += rc1.SizeCustomParameters();
     expected_rate_constants[i_cell][1] = rc2.Calculate(state.conditions_[i_cell], param_iter);
     param_iter += rc2.SizeCustomParameters();
-    expected_rate_constants[i_cell][2] = rc3.Calculate(state.conditions_[i_cell], param_iter);
+    expected_rate_constants[i_cell][2] = rc3.Calculate(state.conditions_[i_cell], param_iter) * (state.conditions_[i_cell].air_density_ * 0.82);
     param_iter += rc3.SizeCustomParameters();
   }
+
   Process::CalculateRateConstants(processes, state);
 
   for (std::size_t i_cell = 0; i_cell < number_of_grid_cells; ++i_cell)
@@ -260,20 +270,17 @@ TEST(Process, PhaseTransferProcessCopyAssignmentSucceeds)
       phase_transfer.process_);
 }
 
-// TODO (Jiwon): This should throw, but currently does not.
-//               I don't think the base class Process should know about a derived-class's specific condition.
-//               Feels like a design issue — will revisit later. Commented out for now.
-//               issue: https://github.com/NCAR/micm/issues/810
-// TEST(Process, SurfaceRateConstantOnlyHasOneReactant)
-// {
-//   Species c("c", { { "molecular weight [kg mol-1]", 0.025 }, { "diffusion coefficient [m2 s-1]", 2.3e2 } });
-//   Species e("e");
+TEST(Process, SurfaceRateConstantOnlyHasOneReactant)
+{
+  Species c("c", { { "molecular weight [kg mol-1]", 0.025 }, { "diffusion coefficient [m2 s-1]", 2.3e2 } });
+  Species e("e");
+  Phase gas_phase { "gas", std::vector<micm::Species>{ c, e } };
 
-//   EXPECT_ANY_THROW(Process r = ChemicalReactionBuilder()
-//                                          .SetPhase(gas_phase)
-//                                          .SetReactants({ c, c })
-//                                          .SetProducts({ Yield(e, 1) })
-//                                          .SetRateConstant(SurfaceRateConstant(
-//                                              { .label_ = "c", .species_ = c, .reaction_probability_ = 0.90 }))
-//                                          .Build(););
-// }
+  EXPECT_ANY_THROW(Process r = ChemicalReactionBuilder()
+                                  .SetReactants({ c, c })
+                                  .SetProducts({ Yield(e, 1) })
+                                  .SetRateConstant(SurfaceRateConstant(
+                                      { .label_ = "c", .species_ = c, .reaction_probability_ = 0.90 }))
+                                  .SetPhase(gas_phase)
+                                  .Build(););
+}
