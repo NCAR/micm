cc68545f94af70146ffab6812057a1935789c356
Jiwon Gim <55209567+boulderdaze@users.noreply.github.com>
2025-12-10 09:06:41 -0700

    Update the version to 3.11.0 (#885)
    update the minor version

--- include/micm/version.hpp
+++ include/micm/version.hpp
@@ -10,7 +10,7 @@ extern "C" {
 
   inline const char* GetMicmVersion()
   {
-    return "3.10.0";
+    return "3.11.0";
   }
   inline unsigned GetMicmVersionMajor()
   {
@@ -18,7 +18,7 @@ extern "C" {
   }
   inline unsigned GetMicmVersionMinor()
   {
-    return 10+0;
+    return 11+0;
   }
   inline unsigned GetMicmVersionPatch()
   {
