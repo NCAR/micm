90f592e524d27ecf7bb25676dd3d0201a8fcbd13
Jiwon Gim <jiwongim@ucar.edu>
2026-01-13 11:48:29 -0700

    address the code review

--- include/micm/util/utils.hpp
+++ include/micm/util/utils.hpp
@@ -4,22 +4,28 @@
 
 #include <string>
 #include <vector>
+#include <format>
 
 namespace micm
 {
-  inline std::string JoinStrings(const std::vector<std::string>& names)
-  {
+inline std::string JoinStrings(const std::vector<std::string>& names)
+{
     std::string result;
-    for (size_t i = 0; i < names.size(); ++i)
+    bool first = true;
+
+    for (const auto& name : names)
     {
-      if (!names[i].empty())
-      {
-        if (!result.empty())
-          result += ".";
-        result += names[i];
-      }
+        if (name.empty())
+            continue;
+
+        if (!first)
+            std::format_to(std::back_inserter(result), ".");
+
+        std::format_to(std::back_inserter(result), "{}", name);
+        first = false;
     }
+
     return result;
-  }
+}
 
 }  // namespace micm
