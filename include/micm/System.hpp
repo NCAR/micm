ac79fc218b0118c03d538d6eea83a3bf090edde4
Jiwon Gim <jiwongim@ucar.edu>
2026-01-14 22:32:31 -0700

    update 2025 to 2026

--- include/micm/System.hpp
+++ include/micm/System.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2024-2025 University Corporation for Atmospheric Research
+// Copyright (C) 2024-2026 University Corporation for Atmospheric Research
 // SPDX-License-Identifier: Apache-2.0
 
 #pragma once
