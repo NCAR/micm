ba5ed886ac28ae713833c8bed0153c8d9a21003e
Jiwon Gim <55209567+boulderdaze@users.noreply.github.com>
2026-04-02 13:30:34 -0600

    Release 3.12.0 (#961)
    update the version

--- include/micm/version.hpp
+++ include/micm/version.hpp
@@ -10,7 +10,7 @@ extern "C" {
 
   inline const char* GetMicmVersion()
   {
-    return "3.11.0";
+    return "3.12.0";
   }
   inline unsigned GetMicmVersionMajor()
   {
@@ -18,7 +18,7 @@ extern "C" {
   }
   inline unsigned GetMicmVersionMinor()
   {
-    return 11+0;
+    return 12+0;
   }
   inline unsigned GetMicmVersionPatch()
   {
