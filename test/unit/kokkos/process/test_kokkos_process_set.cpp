64bf48c695c2baeb2fbe809e2ac38021d1f63706
bbakernoaa <barry.baker@noaa.gov>
2026-04-17 09:14:12 -0400

    feat(kokkos): Add SetAlgebraicVariableIds method and optimize forcing/Jacobian indexing
    - Add SetAlgebraicVariableIds method to KokkosProcessSet for setting algebraic variable flags on device
    - Optimize forcing term indexing to use number_of_forcing_species instead of number_of_species
    - Optimize Jacobian term indexing by pre-computing jacobian_group_size and jac_group_offset to reduce redundant calculations
    - Refactor Jacobian index computation to use pre-computed offset for better performance
    - Update test file with copyright header and consolidate test utilities using shared test policy
    - Improve memory access patterns in AddForcingTerms and SubtractJacobianTerms kernels

--- test/unit/kokkos/process/test_kokkos_process_set.cpp
+++ test/unit/kokkos/process/test_kokkos_process_set.cpp
@@ -1,62 +1,76 @@
-#include <gtest/gtest.h>
+// Copyright (C) 2023-2026 University Corporation for Atmospheric Research
+// SPDX-License-Identifier: Apache-2.0
+
+#include "../../process/test_process_set_policy.hpp"
+
 #include <micm/kokkos/process/kokkos_process_set.hpp>
 #include <micm/kokkos/util/kokkos_dense_matrix.hpp>
 #include <micm/kokkos/util/kokkos_sparse_matrix.hpp>
-#include <micm/process/process.hpp>
-#include <micm/process/chemical_reaction_builder.hpp>
-#include <micm/process/rate_constant/arrhenius_rate_constant.hpp>
 
-TEST(KokkosProcessSet, ForcingTerms)
+#include <gtest/gtest.h>
+
+using KokkosDenseMatrix1 = micm::KokkosDenseMatrix<double, 1>;
+using KokkosDenseMatrix2 = micm::KokkosDenseMatrix<double, 2>;
+using KokkosDenseMatrix3 = micm::KokkosDenseMatrix<double, 3>;
+using KokkosDenseMatrix4 = micm::KokkosDenseMatrix<double, 4>;
+
+using KokkosSparseMatrix1 = micm::KokkosSparseMatrix<double, micm::SparseMatrixVectorOrdering<1>>;
+using KokkosSparseMatrix2 = micm::KokkosSparseMatrix<double, micm::SparseMatrixVectorOrdering<2>>;
+using KokkosSparseMatrix3 = micm::KokkosSparseMatrix<double, micm::SparseMatrixVectorOrdering<3>>;
+using KokkosSparseMatrix4 = micm::KokkosSparseMatrix<double, micm::SparseMatrixVectorOrdering<4>>;
+
+TEST(KokkosProcessSet, VectorMatrix)
 {
-  micm::Species a("A"), b("B"), c("C");
-  micm::Phase gas_phase{ "gas", std::vector<micm::PhaseSpecies>{ a, b, c } };
+  testProcessSet<
+      KokkosDenseMatrix1,
+      KokkosSparseMatrix1,
+      micm::KokkosProcessSet<KokkosDenseMatrix1, KokkosSparseMatrix1>>();
+  testProcessSet<
+      KokkosDenseMatrix2,
+      KokkosSparseMatrix2,
+      micm::KokkosProcessSet<KokkosDenseMatrix2, KokkosSparseMatrix2>>();
+  testProcessSet<
+      KokkosDenseMatrix3,
+      KokkosSparseMatrix3,
+      micm::KokkosProcessSet<KokkosDenseMatrix3, KokkosSparseMatrix3>>();
+  testProcessSet<
+      KokkosDenseMatrix4,
+      KokkosSparseMatrix4,
+      micm::KokkosProcessSet<KokkosDenseMatrix4, KokkosSparseMatrix4>>();
+}
 
-  micm::Process r1 = micm::ChemicalReactionBuilder()
-    .SetReactants({ a, b })
-    .SetProducts({ micm::StoichSpecies(c, 1.0) })
-    .SetRateConstant(micm::ArrheniusRateConstant({ .A_ = 2.0 }))
-    .SetPhase(gas_phase)
-    .Build();
+TEST(KokkosRandomProcessSet, VectorMatrix)
+{
+  testRandomSystem<
+      KokkosDenseMatrix1,
+      KokkosSparseMatrix1,
+      micm::KokkosProcessSet<KokkosDenseMatrix1, KokkosSparseMatrix1>>(200, 50, 40);
+  testRandomSystem<
+      KokkosDenseMatrix1,
+      KokkosSparseMatrix1,
+      micm::KokkosProcessSet<KokkosDenseMatrix1, KokkosSparseMatrix1>>(300, 30, 20);
+  testRandomSystem<
+      KokkosDenseMatrix1,
+      KokkosSparseMatrix1,
+      micm::KokkosProcessSet<KokkosDenseMatrix1, KokkosSparseMatrix1>>(400, 100, 80);
+}
 
-  std::vector<micm::Process> processes{ r1 };
-  std::unordered_map<std::string, std::size_t> variable_map{ {"A", 0}, {"B", 1}, {"C", 2} };
-
-  micm::KokkosProcessSet<micm::KokkosDenseMatrix<double>, micm::KokkosSparseMatrix<double>> process_set(processes, variable_map);
-
-  micm::KokkosDenseMatrix<double> state_variables(1, 3);
-  state_variables[0][0] = 1.0;
-  state_variables[0][1] = 0.5;
-  state_variables[0][2] = 0.0;
-  state_variables.CopyToDevice();
-
-  micm::KokkosDenseMatrix<double> rate_constants(1, 1);
-  rate_constants[0][0] = 2.0;
-  rate_constants.CopyToDevice();
-
-  micm::KokkosDenseMatrix<double> forcing(1, 3, 0.0);
-  forcing.CopyToDevice();
-
-  // Make sure to sync all data to device
-  state_variables.CopyToDevice();
-  rate_constants.CopyToDevice();
-  forcing.CopyToDevice();
-  Kokkos::fence();
-
-  micm::State<micm::KokkosDenseMatrix<double>, micm::KokkosSparseMatrix<double>> state(
-    micm::StateParameters{ .number_of_rate_constants_ = 1, .variable_names_{ "A", "B", "C" } }, 1);
-  state.rate_constants_ = rate_constants;
-  state.rate_constants_.CopyToDevice();
-
-  process_set.AddForcingTerms(state, state_variables, forcing);
-
-  forcing.CopyToHost();
-
-
-  // Rate = 2.0 * 1.0 * 0.5 = 1.0
-  // d[A]/dt = -1.0
-  // d[B]/dt = -1.0
-  // d[C]/dt = 1.0
-  EXPECT_NEAR(forcing[0][0], -1.0, 1e-8);
-  EXPECT_NEAR(forcing[0][1], -1.0, 1e-8);
-  EXPECT_NEAR(forcing[0][2], 1.0, 1e-8);
+TEST(KokkosProcessSetAlgebraicVariables, VectorMatrix)
+{
+  testAlgebraicMasking<
+      KokkosDenseMatrix1,
+      KokkosSparseMatrix1,
+      micm::KokkosProcessSet<KokkosDenseMatrix1, KokkosSparseMatrix1>>();
+  testAlgebraicMasking<
+      KokkosDenseMatrix2,
+      KokkosSparseMatrix2,
+      micm::KokkosProcessSet<KokkosDenseMatrix2, KokkosSparseMatrix2>>();
+  testAlgebraicMasking<
+      KokkosDenseMatrix3,
+      KokkosSparseMatrix3,
+      micm::KokkosProcessSet<KokkosDenseMatrix3, KokkosSparseMatrix3>>();
+  testAlgebraicMasking<
+      KokkosDenseMatrix4,
+      KokkosSparseMatrix4,
+      micm::KokkosProcessSet<KokkosDenseMatrix4, KokkosSparseMatrix4>>();
 }
