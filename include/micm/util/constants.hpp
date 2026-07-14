2daeda2e7a22d564a563d7e841d61af02138eea3
Kyle Shores <kyle.shores44@gmail.com>
2026-07-14 11:12:36 -0500

    concat namespaces

--- include/micm/util/constants.hpp
+++ include/micm/util/constants.hpp
@@ -2,12 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
-namespace micm
-{
-  namespace constants
+
+  namespace micm::constants
   {
     static constexpr double BOLTZMANN_CONSTANT = 1.380649e-23;                      // J K^{-1}
     static constexpr double AVOGADRO_CONSTANT = 6.02214076e23;                      // # mol^{-1}
     static constexpr double GAS_CONSTANT = BOLTZMANN_CONSTANT * AVOGADRO_CONSTANT;  // J K^{-1} mol^{-1}
-  }  // namespace constants
-}  // namespace micm
+  } // namespace micm::constants
+
