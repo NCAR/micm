8b858b7b847563adb3e2e38f9b4572495f57ba29
github-action-benchmark <github@users.noreply.github.com>
2026-08-11 21:26:42 +0000

    add TS1 Instruction Counts (customSmallerIsBetter) benchmark result for 40f2019d70d50f51ab8132ef5f9af87eb94a725d

--- dev/bench/ts1/instructions/data.js
+++ dev/bench/ts1/instructions/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786479139132,
+  "lastUpdate": 1786483602469,
   "repoUrl": "https://github.com/NCAR/micm",
   "entries": {
     "TS1 Instruction Counts": [
@@ -23,6 +23,60 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/NCAR/micm/commit/36d59591a047f0b78695977badf90b3b1beb7619"
         },
         "date": 1786479138273,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "standard",
+            "value": 42091902505,
+            "unit": "instructions"
+          },
+          {
+            "name": "vector1",
+            "value": 42297342364,
+            "unit": "instructions"
+          },
+          {
+            "name": "vector2",
+            "value": 28138463861,
+            "unit": "instructions"
+          },
+          {
+            "name": "vector4",
+            "value": 22005221525,
+            "unit": "instructions"
+          },
+          {
+            "name": "vector8",
+            "value": 18514826722,
+            "unit": "instructions"
+          },
+          {
+            "name": "vector128",
+            "value": 14645751568,
+            "unit": "instructions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "40f2019d70d50f51ab8132ef5f9af87eb94a725d",
+          "message": "Auto-format code changes (#1058)\n\nAuto-format code using Clang-Format\n\nCo-authored-by: GitHub Actions <actions@github.com>",
+          "timestamp": "2026-08-11T15:35:40-05:00",
+          "tree_id": "bc3e2a66d1e5a8d363a6b118b24992b9f856e873",
+          "url": "https://github.com/NCAR/micm/commit/40f2019d70d50f51ab8132ef5f9af87eb94a725d"
+        },
+        "date": 1786483601623,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
