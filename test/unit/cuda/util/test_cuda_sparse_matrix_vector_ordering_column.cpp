a16b6b29c0cf3885c5827b6a240ae4021be27d1f
Matt Dawson <matt@cohere-llc.com>
2026-08-18 15:33:52 -0700

    remove commented out tests

--- test/unit/cuda/util/test_cuda_sparse_matrix_vector_ordering_column.cpp
+++ test/unit/cuda/util/test_cuda_sparse_matrix_vector_ordering_column.cpp
@@ -26,31 +26,6 @@ TEST(SparseVectorCompressedColumnMatrix, SetScalar)
   TestSetScalar<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<3>>();
 }
 
-// CudaSparseMatrix doesn't support AddToDiagonal(), which the next three tests use
-
-// TEST(SparseVectorCompressedColumnMatrix, AddToDiagonal)
-// {
-//   TestAddToDiagonal<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<1>>();
-//   TestAddToDiagonal<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<3>>();
-//   TestAddToDiagonal<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<4>>();
-// }
-
-// TEST(SparseVectorCompressedColumnMatrix, Print)
-// {
-//   TestPrint<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<1>>();
-//   TestPrint<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<2>>();
-//   TestPrint<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<3>>();
-//   TestPrint<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<4>>();
-// }
-
-// TEST(SparseVectorCompressedColumnMatrix, PrintNonZero)
-// {
-//   TestPrintNonZero<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<1>>();
-//   TestPrintNonZero<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<2>>();
-//   TestPrintNonZero<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<3>>();
-//   TestPrintNonZero<micm::CudaSparseMatrix, micm::SparseMatrixVectorOrderingCompressedSparseColumn<4>>();
-// }
-
 /* These are the customized Tests for GPU only */
 
 TEST(CudaSparseMatrix, CopyAssignmentZeroMatrixAddOne)
