b304063e2f39ea51f1f124b40d206b803925eea7
github-action-benchmark <github@users.noreply.github.com>
2026-08-21 20:59:58 +0000

    add Chapman Wall-Clock Timing (customSmallerIsBetter) benchmark result for e2e21ed02e38f42a5368752a28f9ed32affc9b83

--- dev/bench/timing/data.js
+++ dev/bench/timing/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787340023784,
+  "lastUpdate": 1787345998125,
   "repoUrl": "https://github.com/NCAR/micm",
   "entries": {
     "Chapman Wall-Clock Timing": [
@@ -593,6 +593,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "vector128",
             "value": 453.1,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sunjian@ucar.edu",
+            "name": "Jian Sun",
+            "username": "sjsprecious"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e2e21ed02e38f42a5368752a28f9ed32affc9b83",
+          "message": "Fix build, CI and packaging for Kokkos deps (#1075)\n\n* fix build, ci and packaging\n\n* fix the broken CI tests",
+          "timestamp": "2026-08-21T14:55:43-06:00",
+          "tree_id": "87ed9a7dc6ed9621c2458bc09e1421cebf3a6bd9",
+          "url": "https://github.com/NCAR/micm/commit/e2e21ed02e38f42a5368752a28f9ed32affc9b83"
+        },
+        "date": 1787345997386,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "standard",
+            "value": 1236.65,
+            "unit": "ms"
+          },
+          {
+            "name": "vector1",
+            "value": 1256.97,
+            "unit": "ms"
+          },
+          {
+            "name": "vector2",
+            "value": 789.72,
+            "unit": "ms"
+          },
+          {
+            "name": "vector4",
+            "value": 543.67,
+            "unit": "ms"
+          },
+          {
+            "name": "vector8",
+            "value": 461.35,
+            "unit": "ms"
+          },
+          {
+            "name": "vector128",
+            "value": 429.62,
             "unit": "ms"
           }
         ]
