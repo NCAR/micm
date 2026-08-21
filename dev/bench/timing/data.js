418aa417baf4852a56f092bf8cd3de6611e0e70d
github-action-benchmark <github@users.noreply.github.com>
2026-08-21 02:44:01 +0000

    add Chapman Wall-Clock Timing (customSmallerIsBetter) benchmark result for f94aeddd622bba69cffe955449c0c60c2691ba7c

--- dev/bench/timing/data.js
+++ dev/bench/timing/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787271001639,
+  "lastUpdate": 1787280240996,
   "repoUrl": "https://github.com/NCAR/micm",
   "entries": {
     "Chapman Wall-Clock Timing": [
@@ -431,6 +431,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "vector128",
             "value": 259.32,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f94aeddd622bba69cffe955449c0c60c2691ba7c",
+          "message": "Auto-format code changes (#1071)\n\nAuto-format code using Clang-Format\n\nCo-authored-by: GitHub Actions <actions@github.com>",
+          "timestamp": "2026-08-20T20:39:44-06:00",
+          "tree_id": "75f5bb50d306515e6114d38bf5a320d834b3c1ce",
+          "url": "https://github.com/NCAR/micm/commit/f94aeddd622bba69cffe955449c0c60c2691ba7c"
+        },
+        "date": 1787280240334,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "standard",
+            "value": 1246.05,
+            "unit": "ms"
+          },
+          {
+            "name": "vector1",
+            "value": 1251.79,
+            "unit": "ms"
+          },
+          {
+            "name": "vector2",
+            "value": 791.7,
+            "unit": "ms"
+          },
+          {
+            "name": "vector4",
+            "value": 575.05,
+            "unit": "ms"
+          },
+          {
+            "name": "vector8",
+            "value": 469.12,
+            "unit": "ms"
+          },
+          {
+            "name": "vector128",
+            "value": 451.3,
             "unit": "ms"
           }
         ]
