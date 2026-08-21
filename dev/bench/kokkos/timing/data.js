36be0627e5388db49b970e59d3e59c309dad4d40
github-action-benchmark <github@users.noreply.github.com>
2026-08-21 03:36:55 +0000

    add TS1 Timing (Kokkos backend) (customSmallerIsBetter) benchmark result for f94aeddd622bba69cffe955449c0c60c2691ba7c

--- dev/bench/kokkos/timing/data.js
+++ dev/bench/kokkos/timing/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787282960250,
+  "lastUpdate": 1787283415026,
   "repoUrl": "https://github.com/NCAR/micm",
   "entries": {
     "Chapman Timing (Kokkos backend)": [
@@ -148,6 +148,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "vector128",
             "value": 33183.46,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f94aeddd622bba69cffe955449c0c60c2691ba7c",
+          "message": "Auto-format code changes (#1071)\n\nAuto-format code using Clang-Format\n\nCo-authored-by: GitHub Actions <actions@github.com>",
+          "timestamp": "2026-08-20T20:39:44-06:00",
+          "tree_id": "75f5bb50d306515e6114d38bf5a320d834b3c1ce",
+          "url": "https://github.com/NCAR/micm/commit/f94aeddd622bba69cffe955449c0c60c2691ba7c"
+        },
+        "date": 1787283414306,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "vector1",
+            "value": 52056.88,
+            "unit": "ms"
+          },
+          {
+            "name": "vector2",
+            "value": 29399.02,
+            "unit": "ms"
+          },
+          {
+            "name": "vector4",
+            "value": 17451.33,
+            "unit": "ms"
+          },
+          {
+            "name": "vector8",
+            "value": 11595.39,
+            "unit": "ms"
+          },
+          {
+            "name": "vector128",
+            "value": 12856.09,
             "unit": "ms"
           }
         ]
