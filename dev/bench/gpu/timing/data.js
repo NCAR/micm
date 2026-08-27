a5a9f6af3c58ded35f907d06db167a85d6c609a7
github-action-benchmark <github@users.noreply.github.com>
2026-08-27 15:41:31 +0000

    add TS1 Timing (GPU backend) (customSmallerIsBetter) benchmark result for 0b489c8a6098665e03a5e129a3c2aedd22b06e1e

--- dev/bench/gpu/timing/data.js
+++ dev/bench/gpu/timing/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787844499041,
+  "lastUpdate": 1787845291612,
   "repoUrl": "https://github.com/NCAR/micm",
   "entries": {
     "Chapman Timing (GPU runner)": [
@@ -900,6 +900,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "vector128",
             "value": 5042.53,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0b489c8a6098665e03a5e129a3c2aedd22b06e1e",
+          "message": "Auto-format code changes (#1086)\n\nAuto-format code using Clang-Format\n\nCo-authored-by: GitHub Actions <actions@github.com>",
+          "timestamp": "2026-08-27T09:12:35-06:00",
+          "tree_id": "8e37e9c287aac11bfadfce2b73e4c96b712d4d57",
+          "url": "https://github.com/NCAR/micm/commit/0b489c8a6098665e03a5e129a3c2aedd22b06e1e"
+        },
+        "date": 1787845290923,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "vector1",
+            "value": 120745.57,
+            "unit": "ms"
+          },
+          {
+            "name": "vector2",
+            "value": 49950.15,
+            "unit": "ms"
+          },
+          {
+            "name": "vector4",
+            "value": 24419.61,
+            "unit": "ms"
+          },
+          {
+            "name": "vector8",
+            "value": 13254.51,
+            "unit": "ms"
+          },
+          {
+            "name": "vector128",
+            "value": 9191.2,
             "unit": "ms"
           }
         ]
